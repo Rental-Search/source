0847d7be3928c4ebf996ced6e9c8a11278da381e
Timothée Peignier <timothee.peignier@tryphon.org>
2010-08-20 17:07:27 +0200

    add a way to hide an unautorised product

--- eloue/products/manager.py
+++ eloue/products/manager.py
@@ -3,8 +3,8 @@ from django.db.models import Manager
 
 class ProductManager(Manager):
     def active(self):
-        return self.filter(archived=False)
+        return self.filter(archived=False, allowed=True)
     
     def archived(self):
-        return self.filter(archived=True)
+        return self.filter(archived=True, allowed=True)
     
