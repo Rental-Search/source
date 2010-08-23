6566a4b54ede2331503573162905820c2ee704cf
Timothée Peignier <timothee.peignier@tryphon.org>
2010-08-23 14:22:40 +0200

    improve migration script

--- eloue/products/management/commands/migration.py
+++ eloue/products/management/commands/migration.py
@@ -3,11 +3,13 @@ import MySQLdb
 
 from django.template.defaultfilters import slugify
 from django.core.management.base import BaseCommand
+from django.contrib.gis.geos import Point
+from django.contrib.gis.measure import D
 from django.utils.encoding import smart_unicode, smart_str
 
 from geocoders.google import geocoder
 
-from eloue.accounts.models import Patron, Address, PhoneNumber
+from eloue.accounts.models import Patron
 from eloue.products.models import Category
 
 CIVILITY_MAP = {
@@ -39,31 +41,31 @@ COUNTRIES_MAP = {
     'MUS':'MU'
 }
 
+PUBLISH_MAP = { # WARN : We invert value because we mark them as archived rather than published
+    'Y':False,
+    'N':True
+} 
+
 def cleanup_phone_number(phone_number):
     """Cleanup phone number format"""
     return phone_number.replace('O', '0').replace('.', '').replace('o', '').replace(' ', '').replace('/', '')
 
 class Command(BaseCommand):
     def import_category_tree(self, cursor):
-        mapping = {}
         cursor.execute("""SELECT category_id, category_name FROM abs_vm_category""")
         result_set = cursor.fetchall()
         for row in result_set:
             name = smart_unicode(row['category_name'], encoding='latin1') # encode in utf-8
-            category = Category.objects.create(name=name, slug=slugify(name))
-            mapping[category.id] = int(row["category_id"])
-        mapping_inv = dict((v,k) for k, v in mapping.iteritems())
+            category = Category.objects.create(id=row['category_id'], name=name, slug=slugify(name))
         for category in Category.objects.iterator():
-            cursor.execute("""SELECT category_parent_id FROM abs_vm_category_xref WHERE category_child_id = %d""" % mapping[category.id])
+            cursor.execute("""SELECT category_parent_id FROM abs_vm_category_xref WHERE category_child_id = %d""" % category.id)
             row = cursor.fetchone()
             original_parent_id = int(row["category_parent_id"])
             if original_parent_id != 0: # don't add parent to root category 
-                parent_id = mapping_inv[original_parent_id]
-                category.parent_id = parent_id
+                category.parent_id = original_parent_id
                 category.save()
     
     def import_members(self, cursor):
-        # TODO : Add lat, long
         geocode = geocoder('ABQIAAAA7bPNcG5t1-bTyW9iNmI-jRRqVDjnV4vohYMgEqqi0RF2UFYT-xSSwfcv2yfC-sACkmL4FuG-A_bScQ')
         cursor.execute("""SELECT id, username, email, password, registerDate, activation, lat, 'long' FROM abs_users""")
         result_set = cursor.fetchall()
@@ -76,11 +78,11 @@ class Command(BaseCommand):
             lat, lon = row['lat'], row['long']
             
             if not activation_key:
-                patron = Patron.objects.create_user(username, email, password)
+                patron = Patron.objects.create_user(username, email, password, pk=row['id'])
                 patron.date_joined = date_joined
                 patron.save()
             else:
-                patron = Patron.objects.create_inactive(username, email, password, send_email=False) # TODO : THIS IS SENDING AN EMAIL DUDE
+                patron = Patron.objects.create_inactive(username, email, password, send_email=False, pk=row['id']) # TODO : THIS IS SENDING AN EMAIL DUDE
                 patron.date_joined = date_joined
                 patron.save()
             
@@ -103,6 +105,10 @@ class Command(BaseCommand):
                 
                 if address1 and country:
                     name, (lat, lon) = geocode(smart_str("%s %s %s %s" % (address1, address2, zipcode, city)))
+                    if lat and lon:
+                        position = Point(lat, lon)
+                    else:
+                        position = None
                     patron.addresses.create(
                         civility=CIVILITY_MAP[user_info['title']],
                         address1=address1,
@@ -110,8 +116,7 @@ class Command(BaseCommand):
                         zipcode=zipcode,
                         city=city,
                         country=country,
-                        lat=lat,
-                        lon=lon
+                        position=position
                     )
                 
                 if user_info['phone_1']:
@@ -125,9 +130,48 @@ class Command(BaseCommand):
                 
                 patron.save()
     
+    def import_products(self, cursor):
+        # TODO : Missing category and address
+        failures = set()
+        cursor.execute("""SELECT product_id, product_name, product_s_desc, product_desc, count(product_desc) AS quantity, product_full_image, product_publish, prix, caution, vendor_id, product_lat, product_lng, localisation FROM abs_vm_product GROUP BY product_desc, product_name ORDER BY quantity DESC""")
+        result_set = cursor.fetchall()
+        for row in result_set:
+            summary = smart_unicode(row['product_name'], encoding='latin1')
+            if not row['product_desc'] and row['product_s_desc']:
+                description = smart_unicode(row['product_s_desc'], encoding='latin1')
+            else:
+                description = smart_unicode(row['product_desc'], encoding='latin1')
+            
+            vendor_id = row['vendor_id']
+            if vendor_id != 0:
+                owner = Patron.objects.get(pk=vendor_id)
+                if owner.addresses.all():
+                    position = Point(float(row['product_lat']), float(row['product_lng']))
+                    addresses = owner.addresses.filter(position__distance_lte=(position, D(km=25)))
+                    if not addresses:
+                        failures.add(owner.username)
+                
+                cursor.execute("""SELECT category_id FROM abs_vm_product_category_xref WHERE product_id = %s""" % row['product_id'])
+                result = cursor.fetchone()
+                category = Category.objects.get(pk=int(result['category_id']))
+                
+                #product = Product.objects.create(
+                #    summary=summary,
+                #    description=description,
+                #    category=category,
+                #    archived=PUBLISH_MAP[row['product_publish']],
+                #    deposit=row['caution'],
+                #    quantity=row['quantity'],
+                #    owner=owner
+                #)
+        print len(failures)
+            
+            
+    
     def handle(self, *args, **options):
         connection = MySQLdb.connect(unix_socket='/tmp/mysql.sock', user='root', passwd='facteur', db="eloueweb")
         cursor = connection.cursor(MySQLdb.cursors.DictCursor)
         self.import_category_tree(cursor)
         self.import_members(cursor)
+        self.import_products(cursor)
     
