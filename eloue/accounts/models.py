0dd10e754dd609340a16f2db7386318d4b731742
Timothée Peignier <timothee.peignier@tryphon.org>
2010-09-01 12:05:22 +0200

    add some doctest

--- eloue/accounts/models.py
+++ eloue/accounts/models.py
@@ -152,10 +152,18 @@ class Address(models.Model):
     objects = models.GeoManager()
     
     def __unicode__(self):
+        """
+        >>> address = Address(address1='11, rue debelleyme', zipcode='75003', city='Paris')
+        >>> address.__unicode__()
+        u'11, rue debelleyme  75003 Paris'
+        >>> address = Address(address1='11, rue debelleyme', zipcode='75003', city='Paris', position=Point((48.8613232, 2.3631101)))
+        >>> address.__unicode__()
+        u'11, rue debelleyme  75003 Paris (48.8613232, 2.3631101)'
+        """
         if self.position:
-            return smart_unicode("%s %s %s %s (%s, %s)" % (self.address1, self.address2, self.zipcode, self.city, self.position.x, self.position.y))
+            return smart_unicode("%s %s %s %s (%s, %s)" % (self.address1, self.address2 if self.address2 else '', self.zipcode, self.city, self.position.x, self.position.y))
         else:
-            return smart_unicode("%s %s %s %s" % (self.address1, self.address2, self.zipcode, self.city))
+            return smart_unicode("%s %s %s %s" % (self.address1, self.address2 if self.address2 else '', self.zipcode, self.city))
     
     def clean(self):
         from django.core.exceptions import ValidationError
