0e5b1e9eb7a37c4228616dbe95eb579dfb81442c
Timothée Peignier <timothee.peignier@tryphon.org>
2010-09-06 17:55:53 +0200

    dumb ipn handler, need public url or similar to make it work

--- eloue/rent/views.py
+++ eloue/rent/views.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
+import logging
 
+from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.views.decorators.csrf import csrf_exempt
+
+from eloue.rent.models import Booking
+
+log = logging.getLogger(__name__)
+
+@require_POST
+@csrf_exempt
 def ipn_handler(request):
-    pass
+    # FIXME : dumb ipn handler
+    log.info(request.POST)
+    return HttpResponse('OKAY')
