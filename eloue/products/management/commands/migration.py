8b038534358d19a73efaebb2357817ef9c488140
Benoit <benoit.bayszczak@e-loue.com>
2010-09-27 10:18:54 +0200

    adding new categories. close #19

--- eloue/products/management/commands/migration.py
+++ eloue/products/management/commands/migration.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os, MySQLdb
+import csv
 
 from lxml import html
 from optparse import make_option
@@ -10,6 +11,8 @@ from django.core.management.base import BaseCommand
 from django.contrib.gis.geos import Point
 from django.contrib.gis.measure import D
 from django.utils.encoding import smart_unicode
+
+local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
 
 CIVILITY_MAP = {
     'Mr':2,
@@ -39,6 +42,10 @@ COUNTRIES_MAP = {
     'MCO':'MC',
     'MUS':'MU'
 }
+
+CATEGORY_MAP = {}
+csv_file = csv.reader(open(local_path('fixtures/categories_map.csv')))
+CATEGORY_MAP = dict([ (row[1], row[0]) for row in csv_file ])
 
 PUBLISH_MAP = { # WARN : We invert value because we mark them as archived rather than published
     'Y':False,
@@ -88,7 +95,7 @@ class Command(BaseCommand):
 
     def import_category_tree(self, cursor, status=False):
         from eloue.products.models import Category
-        
+
         cursor.execute("""SELECT category_id, category_name FROM abs_vm_category""")
         result_set = cursor.fetchall()
         for row in result_set:
@@ -98,29 +105,29 @@ class Command(BaseCommand):
             cursor.execute("""SELECT category_parent_id FROM abs_vm_category_xref WHERE category_child_id = %d""" % category.id)
             row = cursor.fetchone()
             original_parent_id = int(row["category_parent_id"])
-            if original_parent_id != 0: # don't add parent to root category 
+            if original_parent_id != 0: # don't add parent to root category
                 category.parent_id = original_parent_id
                 category.save()
-    
+
     def import_members(self, cursor, status=False):
         from eloue.accounts.models import Patron
-        
+
         cursor.execute("""SELECT id, username, email, password, registerDate, activation, lat, 'long' FROM abs_users""")
         result_set = cursor.fetchall()
         for i, row in enumerate(result_set):
             if status:
                 print '%s\r' % ' '*20, # clean up row
-            
-            
+
+
             username = smart_unicode(row['username'], encoding='latin1')
             email = smart_unicode(row['email'], encoding='latin1')
             password = smart_unicode("md5$$%s" % row['password'], encoding='latin1')
             date_joined = row['registerDate']
             activation_key = smart_unicode(row['activation'], encoding='latin1')
             lat, lon = row['lat'], row['long']
-            
+
             username = username.lower()
-            
+
             if not activation_key:
                 patron = Patron.objects.create_user(username, email, password, pk=row['id'])
                 patron.date_joined = date_joined
@@ -133,28 +140,28 @@ class Command(BaseCommand):
                 patron.password = password
                 patron.slug = slugify(username)
                 patron.save()
-            
+
             cursor.execute("""SELECT company, title, last_name, first_name, phone_1, phone_2, fax, address_1, address_2, city, country, zip FROM abs_vm_user_info WHERE user_id = %s""" % row['id'])
             user_info = cursor.fetchone()
             if user_info:
                 if user_info['company']:
                     patron.company_name = smart_unicode(user_info['company'], encoding='latin1')
                     patron.is_professional = True
-                
+
                 address1 = smart_unicode(user_info['address_1'], encoding='latin1') or None
                 if address1 and (user_info['address_2'] != '' or user_info['address_2'] != None):
                     address2 = smart_unicode(user_info['address_2'], encoding='latin1')
-                else: 
+                else:
                     address2 = None
-                
+
                 patron.civility = CIVILITY_MAP[user_info['title']]
                 patron.last_name = smart_unicode(user_info['last_name'], encoding='latin1')
                 patron.first_name = smart_unicode(user_info['first_name'], encoding='latin1')
-                
+
                 city = smart_unicode(user_info['city'], encoding='latin1')
                 zipcode = smart_unicode(user_info['zip'], encoding='latin1')
                 country = COUNTRIES_MAP[user_info['country']]
-                
+
                 if address1 and country:
                     patron.addresses.create(
                         address1=address1,
@@ -163,40 +170,40 @@ class Command(BaseCommand):
                         city=city,
                         country=country
                     )
-                
+
                 if user_info['phone_1']:
                     patron.phones.create(number=smart_unicode(cleanup_phone_number(user_info['phone_1']), encoding='latin1'), kind=4)
-                
+
                 if user_info['phone_2']:
                     patron.phones.create(number=smart_unicode(cleanup_phone_number(user_info['phone_2']), encoding='latin1'), kind=4)
-                    
+
                 if user_info['fax']:
                     patron.phones.create(number=smart_unicode(cleanup_phone_number(user_info['fax']), encoding='latin1'), kind=3)
-                
+
                 patron.save()
-            
+
             if status:
                 print ' Migrate members : %d%%' % ((i * 100) / len(result_set)), # note ending with comma
-    
+
     def import_products(self, cursor, status=False):
         from eloue.accounts.models import Patron
         from eloue.products.models import Product, Category
-        
+
         cursor.execute("""SELECT product_id, product_name, product_s_desc, product_desc, count(product_desc) AS quantity, product_full_image, product_publish, prix, caution, vendor_id, product_lat, product_lng, localisation FROM abs_vm_product GROUP BY product_desc, product_name ORDER BY quantity DESC""")
         result_set = cursor.fetchall()
         for i, row in enumerate(result_set):
             if status:
                 print '%s\r' % ' '*20, # clean up row
-            
+
             summary = smart_unicode(row['product_name'], encoding='latin1')
             if not row['product_desc'] and row['product_s_desc']:
                 description = smart_unicode(row['product_s_desc'], encoding='latin1')
             else:
                 description = smart_unicode(row['product_desc'], encoding='latin1')
-            
+
             summary = cleanup_html_entities(summary, method='text')
             description = cleanup_html_entities(description)
-            
+
             vendor_id = int(row['vendor_id'])
             owner = Patron.objects.get(pk=vendor_id)
             if owner.addresses.all():
@@ -204,20 +211,20 @@ class Command(BaseCommand):
                 address = owner.addresses.distance(position).order_by('distance')[0]
             else:
                 print "owner has no addresses : %d" % vendor_id
-            
+
             cursor.execute("""SELECT category_id FROM abs_vm_product_category_xref WHERE product_id = %s""" % row['product_id'])
             result = cursor.fetchone()
             if not result:
                 print "no category for product %d" % row['product_id']
             elif result['category_id'] != 0:
                 try:
-                    category = Category.objects.get(pk=int(result['category_id']))
+                    category = Category.objects.get(name=CATEGORY_MAP[result['category_id']])
                 except Category.DoesNotExist:
                     print "product :", row['product_id'], "category :", result['category_id']
             else:
                 print "no valid category for product %d" % row['product_id']
                 continue
-            
+
             product = Product.objects.create(
                 pk=row['product_id'],
                 summary=summary,
@@ -229,28 +236,28 @@ class Command(BaseCommand):
                 owner=owner,
                 address=address
             )
-            
+
             product.prices.create(
                 unit=1, amount=row['prix'], currency='EUR'
             )
-            
+
             if row['product_full_image']:
                 try: # FIXME : Hardcoded path
                     picture = open(os.path.join('/Users/tim/Downloads/elouefile', str(row['vendor_id']), row['product_full_image']))
                     # product.pictures.create(image=picture)`
                 except IOError, e:
                     pass # print e
-            
+
             product.save()
-            
+
             if status:
                 print ' Migrate products : %d%%' % ((i * 100) / len(result_set)), # note ending with comma
-    
+
     def update_sequence(self):
         db = connection.cursor()
         db.execute("""SELECT setval('auth_user_id_seq', (SELECT MAX(id) FROM auth_user))""")
         db.execute("""SELECT setval('products_product_id_seq', (SELECT MAX(id) FROM products_product))""")
-    
+
     def handle(self, *args, **options):
         mysql_db = MySQLdb.connect(unix_socket='/tmp/mysql.sock', user='root', passwd='facteur', db="eloueweb")
         cursor = mysql_db.cursor(MySQLdb.cursors.DictCursor)
@@ -262,4 +269,4 @@ class Command(BaseCommand):
         if options.get('products'):
             self.import_products(cursor, status)
         self.update_sequence()
-    
+
