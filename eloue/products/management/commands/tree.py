7c7532c20155674f5013723bd8b31c0944f59a59
Timothée Peignier <timothee.peignier@tryphon.org>
2010-10-04 09:57:10 +0200

    improve tree command

--- eloue/products/management/commands/tree.py
+++ eloue/products/management/commands/tree.py
@@ -1,10 +1,14 @@
+# -*- coding: utf-8 -*-
 import yaml
+
 from django.core.management.base import BaseCommand, CommandError
+
+local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
         from eloue.products.models import Category
-        data = yaml.load(open('/home/e-loue/Projets/eloue/eloue/products/management/commands/fixtures/categories.yaml'))
+        data = yaml.load(open(local_path('fixtures/categories.yaml'))
         for section in data:
             root = Category.objects.create(name=section)
             for sub_section in data[section]:
