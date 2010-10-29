235b1c7d5223f51715768c2d5728c20d67ac0965
Timothée Peignier <timothee.peignier@tryphon.org>
2010-10-29 18:01:08 +0200

    dummy views and templates endpoint

--- eloue/rent/views.py
+++ eloue/rent/views.py
@@ -5,6 +5,7 @@ from django.http import HttpResponse
 from django.views.decorators.http import require_POST
 from django.views.decorators.cache import never_cache
 from django.views.decorators.csrf import csrf_exempt
+from django.views.generic.simple import direct_to_template
 
 from eloue.accounts.forms import EmailAuthenticationForm
 from eloue.rent.decorators import validate_ipn
@@ -38,3 +39,11 @@ def pay_ipn(request):
 def booking_create(request, *args, **kwargs):
     wizard = BookingWizard([BookingForm,EmailAuthenticationForm])
     return wizard(request, *args, **kwargs)
+
+
+def booking_success(request):
+    return direct_to_template(request, template="rent/booking_success.html")
+
+
+def booking_failure(request):
+    return direct_to_template(request, template="rent/booking_failure.html")
