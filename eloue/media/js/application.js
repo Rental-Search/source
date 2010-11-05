7c15e68ac8accbdeba7da25ab818f75bdf930f04
Timothée Peignier <timothee.peignier@tryphon.org>
2010-11-05 11:52:17 +0100

    deactivate phone and address field when one is selected. close #56

--- eloue/media/js/application.js
+++ eloue/media/js/application.js
@@ -1,25 +1,58 @@
 $(document).ready(function() {
-    var bool = $("input[name$='exists']:checked").val();
-    var field = $("input[name$='password']");
-    if (field.attr('type') != 'hidden') {
-        if (parseInt(bool, 10)) { 
-            field.removeAttr('disabled');
+    var exists = $("input[name$='exists']:checked").val();
+    var passwordInput = $("input[name$='password']");
+    if (passwordInput.attr('type') != 'hidden') {
+        if (parseInt(exists, 10)) { 
+            passwordInput.removeAttr('disabled');
         } else {
-            field.attr('disabled', 'disabled');
+            passwordInput.attr('disabled', 'disabled');
         }
     }
-
     $("input[name$='exists']").change(function(event) {
-       var radio = event.target;
-       if (field.attr('type') != 'hidden') {
-           if (parseInt(radio.value, 10)) { 
-               field.removeAttr('disabled');
+       var radio = $(event.target);
+       if (passwordInput.attr('type') != 'hidden') {
+           if (parseInt(radio.val(), 10)) { 
+               passwordInput.removeAttr('disabled');
            } else {
-               field.attr('disabled', 'disabled');
+               passwordInput.attr('disabled', 'disabled');
            }
        } 
     });
-
+    
+    var phoneSelect = $("select[name$='phones']");
+    var phoneInput = $("input[name$='phones__phone']");
+    if (phoneSelect.val() && phoneInput.attr('type') != 'hidden') {
+        phoneInput.attr('disabled', 'disabled');
+    }
+    phoneSelect.change(function(event) {
+       var option = $(event.target);
+       if (option.val() && phoneInput.attr('type') != 'hidden') {
+           phoneInput.attr('disabled', 'disabled');
+       } else {
+           phoneInput.removeAttr('disabled');
+       }       
+    });
+    
+    var addressSelect = $("select[name$='addresses']");
+    var addressInput = $(["input[name*='addresses__']", "textarea[name*='addresses__']", "select[name*='addresses__']"]);
+    if (addressSelect.val() && addressInput.attr('type') != 'hidden') {
+        addressInput.each(function(i, el) {
+            $(el).attr('disabled', 'disabled');
+        });
+    }
+    addressSelect.change(function(event) {
+       var option = $(event.target);
+       if (option.val() && addressInput.attr('type') != 'hidden') {
+           addressInput.each(function(i, el) {
+               $(el).attr('disabled', 'disabled');
+           });
+       } else {
+           addressInput.each(function(i, el) {
+               $(el).removeAttr('disabled');
+           });
+       }
+    });
+     
     $('#id_0-started_at_0').datepicker({
         dateFormat: 'dd/mm/yy',
         minDate: 0,
