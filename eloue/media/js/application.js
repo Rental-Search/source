124b11f2b6e50a76c3c01b93e5d99ebc21172546
Timothée Peignier <timothee.peignier@tryphon.org>
2010-11-05 11:03:47 +0100

    improve javascript

--- eloue/media/js/application.js
+++ eloue/media/js/application.js
@@ -1,31 +1,37 @@
-var disableField = function(bool, field) {
-    if (field.attr('type') != "hidden")
-        parseInt(bool) ? field.removeAttr('disabled') : field.attr('disabled', 'disabled');
-}
+$(document).ready(function() {
+    var bool = $("input[name$='exists']:checked").val();
+    var field = $("input[name$='password']");
+    if (field.attr('type') != 'hidden') {
+        if (parseInt(bool, 10)) { 
+            field.removeAttr('disabled');
+        } else {
+            field.attr('disabled', 'disabled');
+        }
+    }
 
-disableField($("input[name='0-exists']:checked").val(), $("input[name='0-password']")), 
-$("input[name='0-exists']").change(function(event) {
-    var radio = event.target;
-    disableField(radio.value, $("input[name='0-password']"));
-});
+    $("input[name$='exists']").change(function(event) {
+       var radio = event.target;
+       if (field.attr('type') != 'hidden') {
+           if (parseInt(radio.value, 10)) { 
+               field.removeAttr('disabled');
+           } else {
+               field.attr('disabled', 'disabled');
+           }
+       } 
+    });
 
-disableField($("input[name='1-exists']:checked").val(), $("input[name='1-password']")), 
-$("input[name='1-exists']").change(function(event) {
-    var radio = event.target;
-    disableField(radio.value, $("input[name='1-password']"));
-});
-
-$('#id_0-started_at_0').datepicker({
-	dateFormat: 'dd/mm/yy',
-	minDate: 0,
-	maxDate: '+360d',
-	onSelect: function(dateText, inst) {
-	    $('#id_0-ended_at_0').val(dateText);
-	    $( "#id_0-ended_at_0" ).datepicker( "option", "minDate", dateText);
-	}
-});
-	
-$('#id_0-ended_at_0').datepicker({
-	dateFormat: 'dd/mm/yy',
-	maxDate: '+85d'
+    $('#id_0-started_at_0').datepicker({
+        dateFormat: 'dd/mm/yy',
+        minDate: 0,
+        maxDate: '+360d',
+        onSelect: function(dateText, inst) {
+            $('#id_0-ended_at_0').val(dateText);
+            $( "#id_0-ended_at_0" ).datepicker( "option", "minDate", dateText);
+        }
+    });
+    
+    $('#id_0-ended_at_0').datepicker({
+        dateFormat: 'dd/mm/yy',
+        maxDate: '+85d'
+    });
 });
