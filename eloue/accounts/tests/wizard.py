be4e98ce97c58c1f308e0ea5052c51b650295a78
Timothée Peignier <timothee.peignier@tryphon.org>
2010-11-05 15:14:44 +0100

    test redirect

--- eloue/accounts/tests/wizard.py
+++ eloue/accounts/tests/wizard.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from urllib import urlencode
 from urlparse import urlsplit
 
 from django.conf import settings
@@ -26,6 +27,20 @@ class AccountWizardTest(TestCase):
             'wizard_step':0
         })
         self.assertRedirects(response, settings.LOGIN_REDIRECT_URL, status_code=301)
+        
+        scheme, netloc, path, query, fragment = urlsplit(response['Location'])
+        redirect_response = response.client.get(path, QueryDict(query))
+        self.assertTrue(redirect_response.context['user'].is_authenticated())
+    
+    def test_first_step_with_redirect(self):
+        args = urlencode({ 'next': reverse('auth_login') })
+        response = self.client.post("%s?%s" % (reverse('auth_login'), args), {
+            '0-email':'alexandre.woog@e-loue.com',
+            '0-exists':1,
+            '0-password':'alexandre',
+            'wizard_step':0
+        })
+        self.assertRedirects(response, reverse('auth_login'), status_code=301)
         
         scheme, netloc, path, query, fragment = urlsplit(response['Location'])
         redirect_response = response.client.get(path, QueryDict(query))
@@ -88,6 +103,23 @@ class AccountWizardTest(TestCase):
         })
         self.assertTrue(response.status_code, 200)
         self.assertContains(response, "Pseudo")
+    
+    def test_second_step(self):
+        response = self.client.post(reverse('auth_login'), {
+            '1-username':'hocus-pocus',
+            '1-password1':'sucop',
+            '1-password2':'sucop',
+            'wizard_step':1,
+            '0-email':'hocus.pocus@e-loue.com',
+            '0-exists':0,
+            '0-password':'',
+            'hash_0':'b5d8e7ffcc52f852c688983ecb30ead6'
+        })
+        self.assertRedirects(response, settings.LOGIN_REDIRECT_URL, status_code=301)
+        
+        scheme, netloc, path, query, fragment = urlsplit(response['Location'])
+        redirect_response = response.client.get(path, QueryDict(query))
+        self.assertTrue(redirect_response.context['user'].is_authenticated())
     
     def tearDown(self):
         settings.SECRET_KEY = self.old_secret_key
