94b529565fb48d19d88335b849b744646526863c
Timothée Peignier <timothee.peignier@tryphon.org>
2010-11-12 12:57:05 +0100

    test contract generator

--- eloue/rent/tests/__init__.py
+++ eloue/rent/tests/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from eloue.rent.tests.contract import *
 from eloue.rent.tests.insurance import *
 from eloue.rent.tests.ipn import *
 from eloue.rent.tests.lean import *
--- eloue/rent/tests/contract.py
+++ eloue/rent/tests/contract.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from pyPdf import PdfFileReader
 from django.test import TestCase
 
 from eloue.rent.contract import ContractGenerator
@@ -10,5 +11,34 @@ class ContractTest(TestCase):
     def test_standard_contract_generator(self):
         booking = Booking.objects.all()[0]
         generator = ContractGenerator()
-        fd = generator(booking)
+        contract = generator(booking)
+        reader = PdfFileReader(contract)
+        self.assertEquals(reader.getNumPages(), 4)
+    
+    def test_first_page(self):
+        booking = Booking.objects.all()[0]
+        generator = ContractGenerator()
+        contract = generator(booking)
+        reader = PdfFileReader(contract)
+        text = reader.getPage(0).extractText()
+        self.assertTrue(booking.owner.get_full_name() in text)
+        self.assertTrue(booking.borrower.get_full_name() in text)
+        self.assertTrue(booking.borrower.get_full_name() in text)
+    
+    def test_second_page(self):
+        booking = Booking.objects.all()[0]
+        generator = ContractGenerator()
+        contract = generator(booking)
+        reader = PdfFileReader(contract)
+        text = reader.getPage(1).extractText()
+        self.assertTrue(booking.product.summary in text)
+        self.assertTrue("%s %s." % (booking.total_amount, booking.currency) in text)
+    
+    def test_third_page(self):
+        booking = Booking.objects.all()[0]
+        generator = ContractGenerator()
+        contract = generator(booking)
+        reader = PdfFileReader(contract)
+        text = reader.getPage(2).extractText()
+        self.assertTrue("%s %s /" % (booking.deposit_amount, booking.currency) in text)
     
