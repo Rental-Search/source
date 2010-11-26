9b9f00d0c34ee8641753c0c3aebb4c0e517efb4b
Timothée Peignier <timothee.peignier@tryphon.org>
2010-11-26 15:33:00 +0100

    add aws credentials in environ

--- wsgi.py
+++ wsgi.py
@@ -3,6 +3,8 @@ import sys, site, os
 local_path = lambda path: os.path.join(os.path.abspath(os.path.dirname(__file__)), path)
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'eloue.settings'
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAJ3PXVVKSTM3WSZSQ'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
 
 site.addsitedir(local_path('env/lib/python2.6/site-packages'))
 
