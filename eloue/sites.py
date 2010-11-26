e4b61de0492c627338a9fa3bca66f34b0012d63f
Timothée Peignier <timothee.peignier@tryphon.org>
2010-11-26 15:45:31 +0100

    fix trouble with aws credentials

--- eloue/sites.py
+++ eloue/sites.py
@@ -1,4 +1,8 @@
 # -*- coding: utf-8 -*-
 import haystack
+import os
+
+os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAJ3PXVVKSTM3WSZSQ'
+os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
 
 haystack.autodiscover()
