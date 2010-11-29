9692ed556eb3957ac72953716df0008a1325ef45
Timothée Peignier <timothee.peignier@tryphon.org>
2010-11-29 11:25:50 +0100

    a less dirty fix

--- eloue/manage.py
+++ eloue/manage.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import os
-os.environ['AWS_ACCESS_KEY_ID'] = 'AKIAJ3PXVVKSTM3WSZSQ'
-os.environ['AWS_SECRET_ACCESS_KEY'] = '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'
-
 from django.core.management import execute_manager
 try:
     import settings  # Assumed to be in the same directory.
+    os.environ['AWS_ACCESS_KEY_ID'] = settings.AWS_ACCESS_KEY_ID
+    os.environ['AWS_SECRET_ACCESS_KEY'] = settings.AWS_SECRET_ACCESS_KEY
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
