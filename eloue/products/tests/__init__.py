efe437570439e80c87bb8416a2f6114f2b2f4522
Timothée Peignier <timothee.peignier@tryphon.org>
2010-12-03 11:14:28 +0100

    only owner now can edit product

--- eloue/products/tests/__init__.py
+++ eloue/products/tests/__init__.py
@@ -2,4 +2,5 @@
 from eloue.products.tests.api import *
 from eloue.products.tests.models import *
 from eloue.products.tests.price import *
+from eloue.products.tests.views import *
 from eloue.products.tests.wizard import *
--- eloue/rent/decorators.py
+++ eloue/rent/decorators.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from django.db import connection
-from django.http import HttpResponseForbidden
 
 
 def incr_sequence(field, sequence_name):
@@ -12,25 +11,5 @@ def incr_sequence(field, sequence_name):
                 row = cursor.fetchone()
                 setattr(self, field, row[0])
             return view_func(self, *args, **kwargs)
-        return inner_wrapper
-    return wrapper
-
-
-def ownership_required(model, object_key='object_id', ownership=None):
-    def wrapper(view_func):
-        def inner_wrapper(request, *args, **kwargs):
-            user = request.user
-            grant = False
-            object_id = kwargs.get(object_key, None)
-            if object_id:
-                names = [(rel.get_accessor_name(), rel.field.name) for rel in user._meta.get_all_related_objects() if rel.model == model]
-                if ownership:
-                    names = filter(lambda name: name[1] in ownership, names)
-                names = map(lambda name: getattr(user, name[0]).filter(pk=object_id).exists(), names)
-                if names:
-                    grant = any(names)
-            if not grant:
-                return HttpResponseForbidden()
-            return view_func(request, *args, **kwargs)
         return inner_wrapper
     return wrapper
