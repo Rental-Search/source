1b9e41aa26c4917a64ea32f6acf66cbfb1308751
Timothée Peignier <timothee.peignier@tryphon.org>
2010-12-04 10:44:48 +0100

    improve booking admin

--- eloue/rent/admin.py
+++ eloue/rent/admin.py
@@ -5,7 +5,9 @@ from eloue.rent.models import Booking
 
 
 class BookingAdmin(admin.ModelAdmin):
-    pass
+    list_filter = ('started_at', 'ended_at', 'state')
+    raw_id_fields = ('owner', 'borrower')
+    list_display = ('product__summary', 'started_at', 'ended_at', 'created_at', 'total_amount')
 
 
 admin.site.register(Booking, BookingAdmin)
