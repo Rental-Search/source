45ef2404482b38778583218c015b87ce3f246495
Timothée Peignier <timothee.peignier@tryphon.org>
2010-12-06 09:40:11 +0100

    fix ended cron. close #113

--- eloue/rent/management/commands/ended.py
+++ eloue/rent/management/commands/ended.py
@@ -3,7 +3,6 @@ import logbook
 
 from datetime import datetime, timedelta
 
-from django.conf import settings
 from django.core.management.base import BaseCommand
 
 log = logbook.Logger('eloue.rent.ended')
@@ -17,10 +16,10 @@ class Command(BaseCommand):
         from eloue.rent.models import Booking
         log.info('Starting hourly ender mover process')
         dtime = datetime.now() + timedelta(hours=1)
-        for booking in Booking.objects.ongoing().filter(started_at__contains=' %02d' % dtime.hour,
-            started_at__day=dtime.day, started_at__month=dtime.month,
-            started_at__year=dtime.year):
-            booking.state=Booking.STATE.ENDED
+        for booking in Booking.objects.ongoing().filter(ended_at__contains=' %02d' % dtime.hour,
+            ended_at__day=dtime.day, ended_at__month=dtime.month,
+            ended_at__year=dtime.year):
+            booking.state = Booking.STATE.ENDED
             booking.send_ended_email()
             booking.save()
         log.info('Finished hourly ender mover process')
