b284dd5584ce7ee9ef3d77b634b5716c657e704b
Timothée Peignier <timothee.peignier@tryphon.org>
2011-01-10 13:49:47 +0100

    add basic configuration for a mobile version

---
+++ eloue/mobile.py
@@ -0,0 +1,12 @@
+import os
+
+from eloue.settings import *
+
+local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
+
+SITE_ID = 3
+
+TEMPLATE_DIRS = (
+    local_path('templates/mobile/'),
+    local_path('templates/'),
+)
