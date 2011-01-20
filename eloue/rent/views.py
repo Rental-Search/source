4afafe6f3c3bb20b55b31d0d9e79ba6b38b15a62
Timothée Peignier <timothee.peignier@tryphon.org>
2011-01-20 16:35:09 +0100

    fix

--- eloue/rent/views.py
+++ eloue/rent/views.py
@@ -21,7 +21,7 @@ from django.views.generic.simple import direct_to_template, redirect_to
 from django_lean.experiments.models import GoalRecord
 from django_lean.experiments.utils import WebUser
 
-from eloue.decorators import ownership_required, validate_ipn
+from eloue.decorators import ownership_required, validate_ipn, secure_required
 from eloue.accounts.forms import EmailAuthenticationForm
 from eloue.products.models import Product
 from eloue.rent.forms import BookingForm, BookingConfirmationForm, BookingStateForm, PreApprovalIPNForm, PayIPNForm, IncidentForm
