619f97f7469c0510c527aa56f62fd31bd54fa03d
Timothée Peignier <timothee.peignier@tryphon.org>
2011-01-21 11:55:25 +0100

    improve mobile configuration

--- eloue/mobile.py
+++ eloue/mobile.py
@@ -6,6 +6,12 @@ local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
 
 SITE_ID = 3
 
+USE_HTTPS = True
+
+SESSION_COOKIE_DOMAIN = 'm.e-loue.com'
+
+CACHE_MIDDLEWARE_KEY_PREFIX = 'mobile'
+
 TEMPLATE_DIRS = (
     local_path('templates/mobile/'),
     local_path('templates/'),
