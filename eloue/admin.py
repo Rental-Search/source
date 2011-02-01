654fe454db6bc890d988803d133c19b8f350fd0d
Timothée Peignier <timothee.peignier@tryphon.org>
2011-02-01 17:36:08 +0100

    remove ugly print

--- eloue/admin.py
+++ eloue/admin.py
@@ -4,7 +4,6 @@ from django.contrib import admin
 
 class CurrentSiteAdmin(admin.ModelAdmin):
     def queryset(self, request):
-        print request.user.is_superuser
         if request.user.is_superuser:
             queryset = self.model.objects.get_query_set()
             ordering = self.ordering or () # otherwise we might try to *None, which is bad ;)
