5c095273cddde1212f1bca52e2c151050a529e42
Timothée Peignier <timothee.peignier@tryphon.org>
2011-02-10 18:10:48 +0100

    translate more urls

--- eloue/products/urls.py
+++ eloue/products/urls.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls.defaults import *
+from django.utils.translation import ugettext as _
 
 from eloue.products.search_indexes import product_search
 from eloue.products.views import product_create, product_list, product_edit
@@ -7,7 +8,7 @@ from eloue.rent.views import booking_create, booking_price
 
 
 urlpatterns = patterns('',
-    url(r'^ajouter/$', product_create, name="product_create"),
+    url(r'^%s/$' % _("ajouter"), product_create, name="product_create"),
     url(r'^(?P<slug>[-\w]+)-(?P<product_id>\d+)/edit/$', product_edit, name="product_edit"),
     url(r'^(?P<slug>[-\w]+)-(?P<product_id>\d+)/price/$', booking_price, name="booking_price"),
     url(r'^(?P<slug>[-\w]+)-(?P<product_id>\d+)/$', booking_create, name="booking_create"),
