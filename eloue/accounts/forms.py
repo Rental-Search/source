cd638924fce0e05f6a9c2d507a36a27c9cb5b5ab
benoitw <benoitwoj@gmail.com>
2011-04-25 09:43:10 +0200

    add tabindex for login form

--- eloue/accounts/forms.py
+++ eloue/accounts/forms.py
@@ -35,9 +35,9 @@ class EmailAuthenticationForm(forms.Form):
     """Displays the login form and handles the login action."""
     exists = forms.TypedChoiceField(required=True, coerce=int, choices=STATE_CHOICES, widget=forms.RadioSelect(renderer=ParagraphRadioFieldRenderer), initial=1)
     email = forms.EmailField(label=_(u"Email"), max_length=75, required=True, widget=forms.TextInput(attrs={
-        'autocapitalize': 'off', 'autocorrect': 'off', 'class': 'inm'
+        'autocapitalize': 'off', 'autocorrect': 'off', 'class': 'inm', 'tabindex': '1'
     }))
-    password = forms.CharField(label=_(u"Password"), widget=forms.PasswordInput(attrs={'class': 'inm'}), required=False)
+    password = forms.CharField(label=_(u"Password"), widget=forms.PasswordInput(attrs={'class': 'inm', 'tabindex': '2'}), required=False)
     
     def __init__(self, *args, **kwargs):
         self.user_cache = None
