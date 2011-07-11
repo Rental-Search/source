2f0ca63d5f73ed905c65a03273229216d9e63e87
benoitw <benoitwoj@gmail.com>
2011-07-11 15:25:22 +0200

    fix bug

--- eloue/media/js/application.js
+++ eloue/media/js/application.js
@@ -59,7 +59,7 @@ $(document).ready(function() {
         }
     });
     
-// Company name field display/none
+    // Company name field display/none
     isProfessionalInput = $("input[name$='is_professional']");
     companyNameInput = $(".company-name");
     companyNameInput.hide();
@@ -316,7 +316,7 @@ $(document).ready(function() {
 	
     });
     
-    //slideshow for iphone page
+    //slideshow for iPhone page
     jQuery(document).ready(function() {
         jQuery('#slideshow').cycle({ 
             delay:  3000, 
@@ -324,7 +324,7 @@ $(document).ready(function() {
             pager: '#nav-slideshow'
         }); 
 
-        function selectMarker() { 
+        function selectMarker() {
             /*jQuery('.slideshow_marker').removeClass('active_marker');
             jQuery('#slideshow_marker_' + this.alt).addClass('active_marker');*/
         };
