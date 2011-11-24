ec6b90ddadc8b6848852247cf5f81cc68ea481d3
benoitw <benoitwoj@gmail.com>
2011-11-24 15:33:54 +0100

    disable submit button after submit facebook button

--- eloue/media/js/application.js
+++ eloue/media/js/application.js
@@ -348,9 +348,10 @@ $(document).ready(function() {
              $('input[name$="facebook_uid"]').val(response.authResponse.userID);
            }
            $('form').submit();
+           $('input[type="submit"]').attr('disabled','disabled');
            $('#fbconnect').remove();
            $('.fbconnect-content span').html('Connexion en cours...');
-         }, {scope: 'email,user_location,offline_access'});
+         }, {scope: 'email,user_location'});
     });
     
 });
