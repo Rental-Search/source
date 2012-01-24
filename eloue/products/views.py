0a31aeae854a945c532eaf0c90b1ad23b34b746d
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-01-24 17:50:09 +0100

    removing commented codes

--- eloue/products/views.py
+++ eloue/products/views.py
@@ -97,29 +97,6 @@ def product_edit(request, slug, product_id):
     )
 
 
-# @login_required
-# def compose_product_related_message(request, recipient=None, form_class=MessageComposeForm,
-#     template_name='django_messages/compose.html', success_url=None, recipient_filter=None):
-#     if request.method == "POST":
-#         sender = request.user
-#         form = form_class(request.POST, recipient_filter=recipient_filter)
-#         if form.is_valid():
-#             form.save(sender=request.user)
-#             messages.add_message(request, messages.SUCCESS, _(u"Message successfully sent."))
-#             if success_url is None:
-#                 success_url = reverse('messages_inbox')
-#             if request.GET.has_key('next'):
-#                 success_url = request.GET['next']
-#             return HttpResponseRedirect(success_url)
-#     else:
-#         form = form_class()
-#         if recipient is not None:
-#             recipients = [u.username for u in User.objects.filter(username__in=[r.strip() for r in recipient.split('+')])]
-#             form.fields['recipient'].initial = ','.join(recipients)
-#     return render_to_response(template_name, {
-#         'form': form,
-#         }, context_instance=RequestContext(request))
-
 def thread_list(user, is_archived):
     return sorted(MessageThread.objects.filter(
       Q(sender=user, sender_archived=is_archived)
