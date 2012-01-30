012be317ebc93ca6898e967075be5e934efad1b1
benoitw <benoitwoj@gmail.com>
2012-01-30 12:08:24 +0100

    new grey label argus.nc

---
+++ eloue/argusnc.py
@@ -0,0 +1,26 @@
+import os
+
+from eloue.settings import *
+local_path = lambda path: os.path.join(os.path.dirname(__file__), path)
+
+SITE_ID = 10
+
+SESSION_COOKIE_DOMAIN = 'argusnc.e-loue.com'
+
+
+USE_HTTPS = False
+
+CACHE_MIDDLEWARE_KEY_PREFIX = 'argusnc'
+
+AFFILIATE_TAG = 'argusnc'
+
+GOOGLE_REGION_CODE = 'nc'
+
+CONVERT_XPF = True
+
+DEFAULT_SITES = [10]
+
+TEMPLATE_DIRS = (
+    local_path('templates/argusnc/'),
+    local_path('templates/'),
+)
