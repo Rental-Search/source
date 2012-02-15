02655e48bcb732b90207c480d2f0f36261f3e083
benoitw <benoitwoj@gmail.com>
2012-02-15 17:38:00 +0100

    init new dashboard template

---
+++ eloue/accounts/templatetags/navbar.py
@@ -0,0 +1,10 @@
+from django.template import Library
+
+register = Library()
+
+@register.simple_tag
+def active(request, pattern):
+    import re
+    if re.search(pattern, request.path):
+        return 'selected-nav-bar-link'
+    return ''
