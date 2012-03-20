9346872ea3414e0e5246260eecb026eaee5be423
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-03-20 11:51:36 +0100

    removing (hopefully) unnecessary double checking from MultiPartFormWizard's __call__

--- eloue/wizard.py
+++ eloue/wizard.py
@@ -65,59 +65,6 @@ class MultiPartFormWizard(FormWizard):
                 prev_fields.append(hidden.render(hash_name, old_data.get(hash_name, self.security_hash(request, old_form))))
         return self.render_template(request, form, ''.join(prev_fields), step, context)
     
-    @method_decorator(csrf_protect)
-    def __call__(self, request, *args, **kwargs):
-        if 'extra_context' in kwargs:
-            self.extra_context.update(kwargs['extra_context'])
-        current_step = self.determine_step(request, *args, **kwargs)
-        self.parse_params(request, *args, **kwargs)
-
-        # Sanity check.
-        if current_step >= self.num_steps():
-            raise Http404('Step %s does not exist' % current_step)
-        
-        # For each previous step, verify the hash and process.
-        for i in range(current_step):
-            form = self.get_form(i, request.POST, request.FILES)
-            if request.POST.get("hash_%d" % i, '') != self.security_hash(request, form):
-                return self.render_hash_failure(request, i)
-            
-            
-            if not form.is_valid():                                             # Hotfix for issue #14498
-                return self.render_revalidation_failure(request, i, form)       # for more details: 
-            else:                                                               # https://code.djangoproject.com/ticket/14498
-                self.process_step(request, form, i)                             #
-        
-        # Process the current step. If it's valid, go to the next step or call
-        # done(), depending on whether any steps remain.
-        if request.method == 'POST':
-            form = self.get_form(current_step, request.POST, request.FILES)
-        else:
-            form = self.get_form(current_step)
-        if form.is_valid():
-            self.process_step(request, form, current_step)
-            next_step = current_step + 1
-            
-            # If this was the last step, validate all of the forms one more
-            # time, as a sanity check, and call done().
-            num = self.num_steps()
-            if next_step == num:
-                final_form_list = [self.get_form(i, request.POST, request.FILES) for i in range(num)]
-                
-                # Validate all the forms. If any of them fail validation, that
-                # must mean the validator relied on some other input, such as
-                # an external Web site.
-                for i, f in enumerate(final_form_list):
-                    if not f.is_valid():
-                        return self.render_revalidation_failure(request, i, f)
-                return self.done(request, final_form_list)
-            
-            # Otherwise, move along to the next step.
-            else:
-                form = self.get_form(next_step)
-                self.step = current_step = next_step
-        return self.render(form, request, current_step)
-    
     def process_step(self, request, form, step):
         super(MultiPartFormWizard, self).process_step(request, form, step)
         if isinstance(form, EmailAuthenticationForm):
