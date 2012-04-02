0b2641f1e33eb3332d80918b9a70c1e00d433ae1
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-04-02 15:10:43 +0200

    done, all tests pass

--- eloue/rent/tests/__init__.py
+++ eloue/rent/tests/__init__.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 from eloue.rent.tests.contract import *
 from eloue.rent.tests.insurance import *
-from eloue.rent.tests.ipn import *
 from eloue.rent.tests.lean import *
 from eloue.rent.tests.models import *
 from eloue.rent.tests.commands import *
