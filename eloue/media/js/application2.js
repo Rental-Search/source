cca495074b92edab093ce08682c3a35ca544020c
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-05-04 17:37:42 +0200

    making unavailable dates unselectable in datepickers

--- eloue/media/js/application2.js
+++ eloue/media/js/application2.js
@@ -233,37 +233,6 @@ $(document).ready(function() {
         bookingPrice($(this));
     });
     
-    //Display datepicker on product detail page
-    startDate = $('input[name$=started_at_0]');
-    startDate.datepicker({
-        dateFormat: 'dd/mm/yy',
-        minDate: 1,
-        maxDate: '+360d',
-        onSelect: function(dateText, inst) {
-            
-            var date1 = $(this).datepicker('getDate');
-            
-            var date = new Date( Date.parse( date1 ) ); 
-            date.setDate( date.getDate() + 1 );
-            
-            var newDate = date.toDateString(); 
-            newDate = new Date( Date.parse( newDate ) );
-            
-            var ended_at = $('input[name$=ended_at_0]');
-
-            ended_at.datepicker("option", "minDate", newDate);
-            
-            ended_at.datepicker('setDate', newDate );
-            
-            bookingPrice($('#booking_create'));
-        }
-    });
-    
-    endDate = $('input[name$=ended_at_0]');
-    endDate.datepicker({
-        dateFormat: 'dd/mm/yy',
-    });
-
     //fbconnect
     $('#fbconnect').click(function () {
         FB.login(function(response) {
