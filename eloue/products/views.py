9167fb737147647216745f1d426a07a774cb1dfd
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-05-04 12:16:22 +0200

    removing commented code from main page view

--- eloue/products/views.py
+++ eloue/products/views.py
@@ -61,30 +61,10 @@ def homepage(request):
     alerts = Alert.on_site.all()[:3]
     try:
         coords = location['coordinates']
-        # region_coords = location.get('region_coords') or coords
-        # region_radius = location.get('region_radius') or location['radius']
         l = Point(coords)
         last_joined = Patron.objects.last_joined_near(l)
-        # last_added = product_search.spatial(
-        #     lat=region_coords[0], long=region_coords[1], radius=min(region_radius, 1541)
-        # ).spatial(
-        #     lat=coords[0], long=coords[1], radius=min(region_radius*2 if region_radius else float('inf'), 1541)
-        # ).order_by('-created_at_date', 'geo_distance')
-        # last_added_car = car_search.spatial(
-        #     lat=region_coords[0], long=region_coords[1], radius=min(region_radius, 1541)
-        # ).spatial(
-        #     lat=coords[0], long=coords[1], radius=min(region_radius*2 if region_radius else float('inf'), 1541)
-        # ).order_by('-created_at_date', 'geo_distance')
-        # last_added_realestate = realestate_search.spatial(
-        #     lat=region_coords[0], long=region_coords[1], radius=min(region_radius, 1541)
-        # ).spatial(
-        #     lat=coords[0], long=coords[1], radius=min(region_radius*2 if region_radius else float('inf'), 1541)
-        # ).order_by('-created_at_date', 'geo_distance')
     except KeyError:
         last_joined = Patron.objects.last_joined()
-        # last_added = product_search.narrow('special:False').order_by('-created_at')
-        # last_added_car = car_search.order_by('-created_at')
-        # last_added_realestate = realestate_search.order_by('-created_at')
     return render_to_response(
         template_name='index.html', 
         dictionary={
