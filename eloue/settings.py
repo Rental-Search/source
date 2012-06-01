28c475ff81cefe13ce62cd94946f36cf0b299489
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-06-01 14:24:09 +0200

    remove beta.e-loue.com from default sites

--- eloue/settings.py
+++ eloue/settings.py
@@ -61,7 +61,7 @@ LANGUAGES = (
 )
 
 SITE_ID = 1
-DEFAULT_SITES = getattr(local, "DEFAULT_SITES", [1, 2, 3, 11])
+DEFAULT_SITES = getattr(local, "DEFAULT_SITES", [1, 2, 3])
 
 # If you set this to False, Django will make some optimizations so as not
 # to load the internationalization machinery.
