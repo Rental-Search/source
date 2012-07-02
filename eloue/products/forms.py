43239a864872cbe22da2b01f0399c82ba1f4d819
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-07-02 16:06:56 +0200

    Fixes #222
    Fixing error when user removes car brand on product edit page

--- eloue/products/forms.py
+++ eloue/products/forms.py
@@ -467,6 +467,8 @@ class CarProductEditForm(ProductEditForm):
         )
 
     def clean(self):
+        if self.errors:
+            return self.cleaned_data
         self.cleaned_data['summary'] = '{brand} - {model}'.format(**self.cleaned_data)
         return self.cleaned_data
 
