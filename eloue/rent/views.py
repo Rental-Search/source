e19f6af03518d4f99d2e17560cdbff3a8f6cd89c
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-07-03 16:48:14 +0200

    remove unused views

--- eloue/rent/views.py
+++ eloue/rent/views.py
@@ -186,39 +186,6 @@ def booking_detail(request, booking_id):
         template_name='rent/booking_detail.html', template_object_name='booking', extra_context={'paypal': paypal})
 
 
-def offer_accept(request, booking_id):
-    booking = get_object_or_404(Booking.on_site, pk=booking_id)
-    if request.user == booking.offer_in_message.recipient:
-        if booking.state == Booking.STATE.UNACCEPTED:
-            booking.state = Booking.STATE.ACCEPTED_UNAUTHORIZED
-            if request.user == booking.borrower:
-                domain = Site.objects.get_current().domain
-                protocol = "https" if USE_HTTPS else "http"
-                booking.preapproval(
-                    cancel_url="%s://%s%s" % (protocol, domain, reverse("booking_failure", args=[booking.pk.hex])),
-                    return_url="%s://%s%s" % (protocol, domain, reverse("booking_success", args=[booking.pk.hex])),
-                    ip_address=request.META['REMOTE_ADDR']
-                )
-            else:
-                #send mail to borrower with paypal link
-                pass
-        else:
-            #we should not be here
-            pass
-    else:
-        return HttpResponseForbidden()
-
-def offer_reject(request, booking_id):
-    booking = get_object_or_404(Booking.on_site, pk=booking_id)
-    if request.user == booking.offer_in_message.recipient:
-        if booking.state == Booking.STATE.UNACCEPTED:
-            booking.state = Booking.STATE.REJECTED
-        else:
-            #we should not be here
-            pass
-    else:
-        return HttpResponseForbidden()
-
 @login_required
 @ownership_required(model=Booking, object_key='booking_id', ownership=['owner', 'borrower'])
 def booking_contract(request, booking_id):
