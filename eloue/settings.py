dce440c28cc31beba11dc51edfb4c439bcce3e2b
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-08-01 15:34:20 +0200

    fixing deprecation warnings for django 1.3

--- eloue/settings.py
+++ eloue/settings.py
@@ -105,7 +105,7 @@ else:
     )
 
 TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.auth',
+    'django.contrib.auth.context_processors.auth',
     'django.core.context_processors.debug',
     'django.core.context_processors.media',
     'django.core.context_processors.request',
