a373c37ba21501727d3f825dc5c95427aa99865e
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-09-18 17:12:44 +0200

    remove last direct_to_template

--- eloue/rent/wizard.py
+++ eloue/rent/wizard.py
@@ -15,7 +15,6 @@ from django.contrib.sites.models import Site
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, redirect
 from django.utils.translation import ugettext_lazy as _
-from django.views.generic.simple import direct_to_template
 
 from eloue.payments.models import PayboxDirectPaymentInformation, PayboxDirectPlusPaymentInformation, NonPaymentInformation
 from eloue.payments.paybox_payment import PayboxManager, PayboxException
