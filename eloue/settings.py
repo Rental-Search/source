e3708e63bd8c6768e6730da9047c29d0b145c0dd
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-09-24 17:34:21 +0200

    we should use pipelinestorage instead of simple staticfilesstorage

--- eloue/settings.py
+++ eloue/settings.py
@@ -347,7 +347,7 @@ AWS_PRELOAD_METADATA = True
 STATIC_ROOT = getattr(local, 'STATIC_ROOT', 'static/')
 STATIC_URL = getattr(local, 'STATIC_URL', '/static/')
 STATICFILES_DIRS = ['eloue/static/', ]
-STATICFILES_STORAGE = getattr(local, 'STATICFILES_STORAGE', 'django.contrib.staticfiles.storage.StaticFilesStorage')
+STATICFILES_STORAGE = getattr(local, 'STATICFILES_STORAGE', 'pipeline.storage.PipelineCachedStorage')
 
 GOOGLE_CLIENT_ID = getattr(local, 'GOOGLE_CLIENT_ID', '218840159400.apps.googleusercontent.com')
 GOOGLE_CLIENT_SECRET = getattr(local, 'GOOGLE_CLIENT_SECRET', 'BXFNFpDb6MN0ocLoPunjkzvZ')
