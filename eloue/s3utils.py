81465267727a99a19087ae75176b1e2e9f546d0e
Balazs KOSSOVICS <balazs.kossovics@e-loue.com>
2012-09-26 10:27:10 +0200

    it was AWS_PRELOAD_METADATA who caused all the problems, removing it allows us to put back CachedFileMixin

--- eloue/s3utils.py
+++ eloue/s3utils.py
@@ -6,17 +6,9 @@ from pipeline.storage import PipelineMixin
 from storages.backends.s3boto import S3BotoStorage
 from boto.utils import parse_ts
 
-class S3PipelineStorage(PipelineMixin, S3BotoStorage):
-    def modified_time(self, name):
-        name = self._normalize_name(self._clean_name(name))
-        entry = self.entries.get(name)
-        if entry is None:
-            entry = self.bucket.get_key(self._encode_name(name))
-
-        # Parse the last_modified string to a local datetime object.
-     	return parse_ts(entry.last_modified)
-
-
+class S3PipelineStorage(PipelineMixin, CachedFilesMixin, S3BotoStorage):
+	pass
+	
 
 StaticRootS3BotoStorage = lambda **kwargs: S3PipelineStorage(
 	location='static', **kwargs)
