309930e3dbb196e26dfc50362961f5a0441bde36
benoitw <benoitwoj@gmail.com>
2012-09-26 09:18:08 +0200

    disable cachefilemixin

--- eloue/s3utils.py
+++ eloue/s3utils.py
@@ -6,7 +6,7 @@ from pipeline.storage import PipelineMixin
 from storages.backends.s3boto import S3BotoStorage
 from boto.utils import parse_ts
 
-class S3PipelineStorage(PipelineMixin, CachedFilesMixin, S3BotoStorage):
+class S3PipelineStorage(PipelineMixin, S3BotoStorage):
     def modified_time(self, name):
         name = self._normalize_name(self._clean_name(name))
         entry = self.entries.get(name)
