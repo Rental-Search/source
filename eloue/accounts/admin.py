cf60964fa88f7e9a7aff507b9efff43c633e11f9
benoitw <benoitwoj@gmail.com>
2013-01-30 16:24:16 +0100

    added link company site in admin account view

--- eloue/accounts/admin.py
+++ eloue/accounts/admin.py
@@ -32,7 +32,7 @@ class PatronAdmin(UserAdmin, CurrentSiteAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'slug', 'password', 'sites')}),
         (_('Personal info'), {'fields': ('civility', 'first_name', 'last_name', 'email', 'affiliate', 'avatar')}),
-        (_('Company info'), {'fields': ('is_professional', 'company_name')}),
+        (_('Company info'), {'fields': ('is_professional', 'company_name', 'url')}),
         (_('Permissions'), {'fields': ('is_staff', 'is_active', 'is_superuser', 'is_subscribed', 'new_messages_alerted', 'user_permissions')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined',)}),
 
