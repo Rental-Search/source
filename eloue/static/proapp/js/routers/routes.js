bb790e0eab8f072c6c9bdda544e81309aae7c963
benoitw <benoitwoj@gmail.com>
2013-06-26 13:42:56 +0200

    keep only stats view

--- eloue/static/proapp/js/routers/routes.js
+++ eloue/static/proapp/js/routers/routes.js
@@ -5,11 +5,7 @@ var app = app || {};
 
 var Workspace = Backbone.Router.extend({
 	routes: {
-		'': 							'home',
 		'stats/(:metric/)': 			'stats',
-		'messages/': 					'messages',
-		'ads/(:id/)(:params/)': 		'ads',
-		'accounts/(:params/)':			'accounts', 
 	},
 
 	initialize: function() {
@@ -20,12 +16,8 @@ var Workspace = Backbone.Router.extend({
 		Backbone.history.start({pushState: true, root: '/pro/dashboard/'})
 	},
 
-	home: function() {
-		app.layoutView.setSelectedNavTabViewAtIndex(0);
-	},
-
 	stats: function(metric) {
-		app.layoutView.setSelectedNavTabViewAtIndex(1);
+		app.layoutView.setSelectedNavTabViewAtIndex(0);
 		
 		if( _.isNull(metric) ) {
 			app.layoutView.selectedNavTabView.setSelectedNavTabViewAtIndex(0);
@@ -37,35 +29,4 @@ var Workspace = Backbone.Router.extend({
 			app.layoutView.selectedNavTabView.setSelectedNavTabViewAtIndex(3);
 		}
 	},
-	messages: function() {
-		app.layoutView.setSelectedNavTabViewAtIndex(2);
-	},
-
-	ads: function(id, params) {
-		app.layoutView.setSelectedNavTabViewAtIndex(3);
-
-		app.layoutView.selectedNavTabView.setDetailViewWithId(id, function() {
-			if( _.isNull(params) ) {
-				app.layoutView.selectedNavTabView.selectedDetailView.setSelectedNavTabViewAtIndex(0);
-			} else if ( params == 'pictures' ) {
-				app.layoutView.selectedNavTabView.selectedDetailView.setSelectedNavTabViewAtIndex(1);
-			} else if ( params == 'prices' ) {
-				app.layoutView.selectedNavTabView.selectedDetailView.setSelectedNavTabViewAtIndex(2);
-			}
-		});
-	},
-	
-	accounts: function(params) {
-		app.layoutView.setSelectedNavTabViewAtIndex(4);
-
-		if( _.isNull(params) ) {
-			app.layoutView.selectedNavTabView.setSelectedNavTabViewAtIndex(0);
-		} else if (params == 'shop') {
-			app.layoutView.selectedNavTabView.setSelectedNavTabViewAtIndex(1);	
-		} else if (params == 'billing') {
-			app.layoutView.selectedNavTabView.setSelectedNavTabViewAtIndex(2);	
-		} else if (params == 'plan') {
-			app.layoutView.selectedNavTabView.setSelectedNavTabViewAtIndex(3);	
-		}
-	}
 });
--- eloue/static/proapp/js/views/layout.js
+++ eloue/static/proapp/js/views/layout.js
@@ -10,44 +10,7 @@ app.LayoutView = app.NavView.extend({
 	initialize: function() {
 		//Extend the class name for navpills
 		this.navTabItemsView = this.navTabItemsView.extend({className:  'nav nav-pills  top-nav'});
-
-		var itemView = app.NavTabItemView.extend({
-			template: _.template($("#navpillsitem-template").html()),
-		});
-
-		var HomeView = Backbone.View.extend({
-			className: 'content-pill',
-
-			item: itemView.extend({path: '', icon: 'home', labelName: 'Acceuil'}),
-
-			render: function() {
-				this.$el.html('<h1>Acceuil</h1>');
-				return this;
-			},
-		});
-
-		var MessagesView = Backbone.View.extend({
-			className: 'content-pill',
-
-			item: itemView.extend({path: 'messages/', icon: 'envelope', labelName: 'Messages'}),
-
-			render: function() {
-				this.$el.html('<h1>Messages</h1>');
-				return this;
-			},
-		});
-
-		var AdsView = Backbone.View.extend({
-			className: 'content-pill',
-
-			item: itemView.extend({path: 'ads/', icon: 'show_thumbnails_with_lines', labelName: 'Annonces'}),
-
-			render: function() {
-				this.$el.html('<h1>Annonces</h1>');
-				return this;
-			},
-		});
 		
-		this.navTabViews = [HomeView, app.StatsPillContentView, MessagesView, app.AdsPillContentView, app.AccountsPillContentView];
+		this.navTabViews = [app.StatsPillContentView];
 	},
 });
