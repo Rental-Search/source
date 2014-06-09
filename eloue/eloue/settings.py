da17524fca49cbfe000459c21d1234f0fba50259
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2014-06-09 14:25:39 +0700

    Fixed 'INTERCEPT_REDIRECTS' is obsoleted since django-debug-panel 1.1.0 issue, and allowed to override DBP from local.py settings module

--- eloue/eloue/settings.py
+++ eloue/eloue/settings.py
@@ -7,9 +7,25 @@ DEBUG = getattr(local, 'DEBUG', False)
 DEBUG_TOOLBAR = getattr(local, 'DEBUG_TOOLBAR', False)
 TEMPLATE_DEBUG = DEBUG
 
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+#    'debug_toolbar.panels.redirects.RedirectsPanel',
+] + getattr(local, 'DEBUG_TOOLBAR_PANELS', [])
+
 DEBUG_TOOLBAR_CONFIG = {
-    'INTERCEPT_REDIRECTS': False,
+    'DISABLE_PANELS': set(),
 }
+DEBUG_TOOLBAR_CONFIG.update(getattr(local, 'DEBUG_TOOLBAR_CONFIG', {}))
 
 ADMINS = (
     ('Operational Team', 'ops@e-loue.com'),
