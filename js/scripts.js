62f3f54a1826f0291b2e10f924dc5e9bd06addb3
aovechkin <anton.ovehckin@noveogroup.com>
2014-07-14 20:44:30 +0700

    sass, bootstrap, jade

--- js/scripts.js
+++
@@ -1,4 +0,0 @@
-$( document ).ready( function() {
-	$( ".product-list" ).find( '.product-item:nth-of-type(4n)' ).css( "margin-right", "0" );
-	console.log( $( ".product-list" ).find( '.products-item:nth-of-type(4n)' ));
-} );
