e50a22875d9409565e263fbef3ba14e6c151e090
Shota Abashiya <sabashiya@noveogroup.com>
2014-08-06 14:04:20 +0700

    Added file change listener

--- angular-multipage/static/js/dashboard/eloue/controllers/account/AccountProfileCtrl.js
+++ angular-multipage/static/js/dashboard/eloue/controllers/account/AccountProfileCtrl.js
@@ -8,16 +8,17 @@ define(["angular", "eloue/app", "../../../../common/eloue/services"], function (
     angular.module("EloueDashboardApp").controller("AccountProfileCtrl", [
         "$scope",
         "UsersService",
-        "Endpoints",
-        function ($scope, UsersService, Endpoints) {
+        function ($scope, UsersService) {
             UsersService.getMe(function (currentUser) {
                 // Save current user in the scope
                 $scope.currentUser = currentUser;
 
-                // Send form by submit
-                $scope.submit = function () {
+                // Send form when a file changes
+                $scope.onFileChanged = function () {
                     UsersService.sendForm($scope.currentUser.id, $("#profile_form"), function (data) {
-                        $scope.currentUser = data;
+                        $scope.$apply(function () {
+                            $scope.currentUser = data;
+                        });
                     });
                 };
             });
--- angular-multipage/static/js/dashboard/eloue/directives/FileChooserDirective.js
+++ angular-multipage/static/js/dashboard/eloue/directives/FileChooserDirective.js
@@ -5,20 +5,17 @@ define(["angular", "eloue/app"], function (angular) {
     /**
      * Controller for the messages page.
      */
-    angular.module("EloueDashboardApp").directive("fileChooser", ["FileReader", "$parse",
-        function (FileReader, $parse) {
-            return {
-                restrict: "A",
-                link: function (scope, element, attrs) {
-                    var urlModel = $parse(attrs.urlModel);
-                    
-                    element.bind("change", function () {
-                        FileReader.readAsDataUrl(element[0].files[0], scope).then(function (result) {
-                            urlModel.assign(scope, result)
-                        });
-                    });
-                }
-            };
-        }
-    ]);
+    angular.module("EloueDashboardApp").directive("fileChooser", [function () {
+        return {
+            restrict: "A",
+            scope: {
+                onChange: "&"
+            },
+            link: function (scope, element, attrs) {
+                element.bind("change", function () {
+                    scope.onChange();
+                });
+            }
+        };
+    }]);
 });
