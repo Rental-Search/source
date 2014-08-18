cd7dfa14925818d762c665900e7a5016b69fb2ba
Shota Abashiya <sabashiya@noveogroup.com>
2014-08-18 11:54:12 +0700

    Addresses: fixed bug with empty default address

--- angular-multipage/static/js/dashboard/eloue/controllers/account/AccountAddressesCtrl.js
+++ angular-multipage/static/js/dashboard/eloue/controllers/account/AccountAddressesCtrl.js
@@ -16,7 +16,7 @@ define(["angular", "eloue/app"], function (angular) {
 
                 AddressesService.getAddressesByPatron(currentUserId).$promise.then(function (data) {
                     $scope.addressList = data.results;
-                    $scope.defaultAddressId = UtilsService.getIdFromUrl(currentUser.default_address);
+                    $scope.defaultAddressId = (!!currentUser.default_address) ? UtilsService.getIdFromUrl(currentUser.default_address) : null;
                     console.log(data.results);
                 });
             });
