513a71724ef7eb3d05cdfb9abc558ec5f3d28f30
aovechkin <anton.ovehckin@noveogroup.com>
2014-08-18 19:18:23 +0700

    сверстал aside в product_list.jade

--- eloue/eloue/static/js/scripts.js
+++ eloue/eloue/static/js/scripts.js
@@ -1,42 +1,70 @@
 $(document).ready(function(){
-	var classic_form = $('.classic-form');
-	classic_form.hide();
-	$('#geolocate').formmapper({
-		details: 'form',
-	});
-	$('.registration').on('click', function(){
-		classic_form.slideDown();
-		$(this).slideUp();
-	});
-	$('select').chosen();
+  var classic_form = $('.classic-form'),
+      article = $('article'),
+      layout_switcher = $('.layout-switcher');
+  classic_form.hide();
+  $('#geolocate').formmapper({
+    details: 'form',
+  });
+  $('.registration').on('click', function(){
+    classic_form.slideDown();
+    $(this).slideUp();
+  });
+  $('select').chosen();
 
-	var slide_imgs = [].slice.call($('.carousel-wrapper').find('img'));
-	for(var index = 0; index < slide_imgs.length; index++) {
-		var proportions = $(slide_imgs[index]).width() / $(slide_imgs[index]).height(),
-				parent = $(slide_imgs[index]).parent(),
-				parent_proportions = $(parent).width() / $(parent).height();
+  var slide_imgs = [].slice.call($('.carousel-wrapper').find('img'));
+  for(var index = 0; index < slide_imgs.length; index++) {
+    var proportions = $(slide_imgs[index]).width() / $(slide_imgs[index]).height(),
+        parent = $(slide_imgs[index]).parent(),
+        parent_proportions = $(parent).width() / $(parent).height();
 
-		if(proportions < parent_proportions) {
-			$(slide_imgs[index]).addClass('expand-v');
-		} else {
-			$(slide_imgs[index]).addClass('expand-h');
-		}
-	};
+    if(proportions < parent_proportions) {
+      $(slide_imgs[index]).addClass('expand-v');
+    } else {
+      $(slide_imgs[index]).addClass('expand-h');
+    }
+  };
 
-	(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
 
-	window.___gcfg = {lang: 'fr'};
-	  (function() {
-	    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-	    po.src = 'https://apis.google.com/js/platform.js';
-	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-	  })();
+  window.___gcfg = {lang: 'fr'};
+    (function() {
+      var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+      po.src = 'https://apis.google.com/js/platform.js';
+      var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+    })();
 
-	!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
+  // switch grid/list layouts
+  $(layout_switcher).on('click', 'i', function(){
+    if($(this).hasClass('sprite-grid')) {
+      article.removeClass('list-layout')
+      article.addClass('grid-layout')
+    } else {
+      article.removeClass('grid-layout')
+      article.addClass('list-layout')
+      
+    }
+  });
+
+   $( "#district-fake" ).slider({
+    range: "min",
+    value: 400,
+    min: 1,
+    max: 700,
+  });
+
+   $( "#price-fake" ).slider({
+    range: true,
+    min: 0,
+    max: 700,
+    values: [100,400],
+  });
+
+  !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
 });
