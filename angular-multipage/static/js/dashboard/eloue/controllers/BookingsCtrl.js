78abc8d165066200d3c7f26157c29731be99be7b
Shota Abashiya <sabashiya@noveogroup.com>
2014-08-24 21:20:32 +0700

    Bookings: added filtration by users

--- angular-multipage/static/js/dashboard/eloue/controllers/BookingsCtrl.js
+++ angular-multipage/static/js/dashboard/eloue/controllers/BookingsCtrl.js
@@ -6,11 +6,37 @@ define(["angular", "eloue/app"], function (angular) {
      * Controller for the bookings page.
      */
     angular.module("EloueDashboardApp").controller("BookingsCtrl", [
+        "$q",
         "$scope",
+        "Endpoints",
         "BookingsLoadService",
-        function ($scope, BookingsLoadService) {
-            BookingsLoadService.getBookingList().then(function (bookingList) {
-                $scope.bookingList = bookingList;
+        function ($q, $scope, Endpoints, BookingsLoadService) {
+            var promises = {
+                currentUser: $scope.currentUserPromise,
+                bookingList: BookingsLoadService.getBookingList()
+            };
+
+            $q.all(promises).then(function (results) {
+                $scope.bookingList = results.bookingList;
+                $scope.bookingFilter = {};
+
+                // Get current user url
+                var currentUserUrl = Endpoints.api_url + "users/" + results.currentUser.id + "/";
+
+                $scope.filterByOwner = function () {
+                    $scope.bookingFilter.owner = currentUserUrl;
+                    $scope.bookingFilter.borrower = undefined;
+                };
+
+                $scope.filterByBorrower = function () {
+                    $scope.bookingFilter.owner = undefined;
+                    $scope.bookingFilter.borrower = currentUserUrl;
+                };
+
+                $scope.filterByBoth = function () {
+                    $scope.bookingFilter.owner = undefined;
+                    $scope.bookingFilter.borrower = undefined;
+                };
 
                 // Initiate custom scrollbars
                 $scope.initCustomScrollbars();
