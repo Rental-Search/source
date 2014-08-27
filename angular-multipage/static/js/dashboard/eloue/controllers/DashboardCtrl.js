ebd44371d96b96db461fdb3501526ddad592da1d
Maxim Baev <mbaev@noveogroup.com>
2014-08-27 12:19:08 +0700

    Started Dashboard main page, profile info.

--- angular-multipage/static/js/dashboard/eloue/controllers/DashboardCtrl.js
+++ angular-multipage/static/js/dashboard/eloue/controllers/DashboardCtrl.js
@@ -6,6 +6,10 @@ define(["angular", "eloue/app"], function (angular) {
      * Controller for the dashboard page.
      */
     angular.module("EloueDashboardApp").controller("DashboardCtrl", ["$scope", function ($scope) {
-        $scope.title = "Dashboard title";
+        $scope.currentUserPromise.then(function (currentUser) {
+            // Save current user in the scope
+            $scope.currentUser = currentUser;
+            console.log($scope.currentUser);
+        });
     }]);
 });
