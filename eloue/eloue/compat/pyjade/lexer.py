7db390c31906c5ec10cd2c08ec5697e478e17d32
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2014-09-02 13:52:43 +0700

    Fixed pyjade '| ..' drops spaces between | and the text

--- eloue/eloue/compat/pyjade/lexer.py
+++ eloue/eloue/compat/pyjade/lexer.py
@@ -5,3 +5,4 @@ from pyjade.lexer import Lexer as _Lexer
 
 class Lexer(_Lexer):
     RE_ASSIGNMENT = re.compile(r'^(-\s*var\s+)?(\w+) += *([^;\n]+)( *;? *)')
+    RE_STRING = re.compile(r'^\|(\s+[^\n]+)')
