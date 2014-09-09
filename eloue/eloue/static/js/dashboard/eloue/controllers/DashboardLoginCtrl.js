056bd0d3d536e99a411c77cdd6a0ec759a9350f8
Maxim Baev <mbaev@noveogroup.com>
2014-09-09 12:22:41 +0700

    Updated login and registration pop-in Jade templates, fixed Dashboard login controller.

--- eloue/eloue/static/js/dashboard/eloue/controllers/DashboardLoginCtrl.js
+++ eloue/eloue/static/js/dashboard/eloue/controllers/DashboardLoginCtrl.js
@@ -6,8 +6,10 @@ define(["angular", "eloue/app"], function (angular) {
      * Controller for the items page.
      */
     angular.module("EloueDashboardApp").controller("DashboardLoginCtrl", [
-        "$scope",
-        function ($scope) {
-            $("#loginModal").modal("show");
+        "$scope", "$timeout",
+        function ($scope, $timeout) {
+            $timeout(function () {
+                $("#loginModal").modal("show");
+            }, 300);
         }]);
 });
