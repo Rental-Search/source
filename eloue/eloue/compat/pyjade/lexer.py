99e781ca081917800e56b85f3876f46805c47c02
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2014-09-19 00:53:28 +0700

    Some more polishing of PyJade lexer's overrided regexps

--- eloue/eloue/compat/pyjade/lexer.py
+++ eloue/eloue/compat/pyjade/lexer.py
@@ -4,5 +4,5 @@ import re
 from pyjade.lexer import Lexer as _Lexer
 
 class Lexer(_Lexer):
-    RE_ASSIGNMENT = re.compile(r'^(-\s*var\s+)?(\w+) += *([^;\n]+)( *;? *)')
-    RE_STRING = re.compile(r'^\|(\s+[^\n]+)')
+    RE_ASSIGNMENT = re.compile(r'^(-\s*var\s+)?(\w+)\s*=\s*([^;\n]+)([\s;]*)')
+    RE_STRING = re.compile(r'^\|([^\n]+)')
