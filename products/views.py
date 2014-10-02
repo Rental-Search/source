d877bfc8fbefdeb58e2f51061e11c4b2701603a5
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2014-10-02 13:49:19 +0700

    263 Float comments - add the product detail link

--- products/views.py
+++ products/views.py
@@ -38,7 +38,7 @@ from products.utils import format_quote, escape_percent_sign
 from products.search import product_search, car_search, realestate_search, product_only_search
 
 from rent.forms import BookingOfferForm
-from rent.models import Booking
+from rent.models import Booking, Comment
 from rent.choices import BOOKING_STATE
 
 from eloue.decorators import ownership_required, secure_required, mobify, cached
@@ -734,6 +734,7 @@ class CommonPageContextMixin(object):
     breadcrumbs = {'sort': {'name': 'sort', 'value': None, 'label': 'sort', 'facet': False},
                    'l': {'name': 'l', 'value': None, 'label': 'l', 'facet': False},
                    }
+
     def get_context_data(self, **kwargs):
         context = {
             'category_list': Category.on_site.filter(pk__in=[35, 390, 253, 418, 2700, 2713, 172, 126, 323]),
@@ -757,6 +758,7 @@ class HomepageView(CommonPageContextMixin, TemplateView):
             'cities_list': product_stats,
             'total_products': Product.objects.only('id').count(),
             'product_list': last_added(product_search, self.location, limit=8),
+            'comment_list': Comment.objects.select_related('booking__product__address').order_by('-created_at')[:1],
         }
 
     def get_context_data(self, **kwargs):
