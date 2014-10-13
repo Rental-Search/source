472b4d1d9e538a1a05585f668f3ee9abaa18569a
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2014-10-13 15:15:31 +0700

    Fixed publish item page after one of recent changes

--- products/views.py
+++ products/views.py
@@ -720,7 +720,21 @@ from eloue.views import AjaxResponseMixin
 from eloue.decorators import ajax_required
 from products.forms import SuggestCategoryViewForm
 
-class HomepageView(BreadcrumbsMixin, TemplateView):
+class NavbarCategoryMixin(object):
+    def get_context_data(self, **kwargs):
+        categories = [
+            253, 335, 35, 390, 126, 418, 2700, 495, # first line / nav bar
+            323, 432, 297, 379, 2713, 512, 3, # others / dropdown selection
+        ]
+        category_list = list(Category.on_site.filter(pk__in=categories))
+        category_list.sort(key=lambda obj: categories.index(obj.pk))
+        context = {
+            'category_list': category_list,
+        }
+        context.update(super(NavbarCategoryMixin, self).get_context_data(**kwargs))
+        return context
+
+class HomepageView(NavbarCategoryMixin, BreadcrumbsMixin, TemplateView):
     template_name = 'index.jade'
 
     @property
@@ -731,23 +745,17 @@ class HomepageView(BreadcrumbsMixin, TemplateView):
             where=['"products_product"."address_id" = "accounts_address"."id"'],
             select={'city': 'lower(trim("accounts_address"."city"))'}
         ).values('city').annotate(Count('id')).order_by('-id__count')
-        categories = [
-            253, 335, 35, 390, 126, 418, 2700, 495, # first line / nav bar
-            323, 432, 297, 379, 2713, 512, 3, # others / dropdown selection
-        ]
-        category_list = list(Category.on_site.filter(pk__in=categories))
-        category_list.sort(key=lambda obj: categories.index(obj.pk))
         return {
             'cities_list': product_stats,
             'total_products': Product.on_site.only('id').count(),
             'product_list': last_added(product_search, self.location, limit=8),
             'comment_list': Comment.objects.select_related('booking__product__address').order_by('-created_at'),
-            'category_list': category_list,
         }
 
     def get_context_data(self, **kwargs):
-        context = super(HomepageView, self).get_context_data(**kwargs)
+        context = {}
         context.update(self.home_context)
+        context.update(super(HomepageView, self).get_context_data(**kwargs))
         return context
 
     def get(self, request, *args, **kwargs):
@@ -795,7 +803,7 @@ class ProductDetailView(SearchQuerySetMixin, DetailView):
         context.update(super(ProductDetailView, self).get_context_data(**kwargs))
         return context
 
-class PublishItemView(BreadcrumbsMixin, TemplateView):
+class PublishItemView(NavbarCategoryMixin, BreadcrumbsMixin, TemplateView):
     template_name = 'publich_item/index.jade'
 
 class SuggestCategoryView(AjaxResponseMixin, View):
