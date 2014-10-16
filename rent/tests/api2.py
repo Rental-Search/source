fc52c9837fd3f8c1f8e4ea798eaa264b90f6f047
Pavel Antonov <pavel.antonov@noveogroup.com>
2014-10-16 14:02:59 +0700

    Unit test for booking incident added

--- rent/tests/api2.py
+++ rent/tests/api2.py
@@ -184,6 +184,31 @@ class BookingTest(APITransactionTestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTrue(response.content.startswith('%PDF'), "'{}'.startswith('%PDF')".format(response.content))
 
+    def test_booking_incident(self):
+        response = self.client.post(_location('booking-list'), {
+            'started_at': datetime.now() + timedelta(days=2),
+            'ended_at': datetime.now() + timedelta(days=4),
+            'product': _location('product-detail', pk=6),
+        })
+        uuid = response.data['uuid']
+
+        response = self.client.put(_location('booking-pay', uuid), {
+            'expires': '0517',
+            'holder_name': 'John Doe',
+            'card_number': '4987654321098769',
+            'cvv': '123',
+        })
+        self.assertEqual(response.status_code, 200, response.data)
+
+        response = self.client.put(_location('booking-accept', uuid))
+        self.assertEqual(response.status_code, 200, response.data)
+
+        response = self.client.put(_location('booking-incident', uuid), {
+            'description': 'Description',
+        })
+        self.assertEqual(response.status_code, 200, response.data)
+        self.assertEqual(response.data['detail'], _(u'Transition performed'))
+
     def test_booking_accept_wrong_state(self):
         response = self.client.post(_location('booking-list'), {
             'started_at': datetime.now() + timedelta(days=2),
