b99763665dc1fbc381df83a4dd17bf95dab4a4e9
Maxim Baev <mbaev@noveogroup.com>
2014-10-17 15:03:24 +0700

    #304 Dashboard account  page:  - Added error and success reset password handling

--- eloue/static/js/dashboard/eloue/controllers/account/AccountPasswordCtrl.js
+++ eloue/static/js/dashboard/eloue/controllers/account/AccountPasswordCtrl.js
@@ -7,16 +7,34 @@ define(["angular", "eloue/app"], function (angular) {
      */
     angular.module("EloueDashboardApp").controller("AccountPasswordCtrl", [
         "$scope",
+        "$state",
+        "$stateParams",
         "UsersService",
-        function ($scope, UsersService) {
+        function ($scope,$state, $stateParams, UsersService) {
             $scope.markListItemAsSelected("account-part-", "account.password");
+
+            $scope.errors = {
+                current_password: "",
+                password: "",
+                confirm_password: ""
+            };
+
             $scope.resetPassword = function () {
                 $scope.submitInProgress = true;
                 if (!!$scope.currentUser) {
                     UsersService.resetPassword($scope.currentUser.id, $("#reset-password-form")).then(function(result) {
-                        $scope.$apply(function () {
-                            $scope.submitInProgress = false;
-                        });
+                        $scope.submitInProgress = false;
+                        $scope.showNotification(result.detail);
+                        $state.transitionTo($state.current, $stateParams, { reload: true });
+                    }, function(error) {
+                        if (!!error.responseJSON && !!error.responseJSON.errors) {
+                            $scope.errors = {
+                                current_password: !!error.responseJSON.errors.current_password ? error.responseJSON.errors.current_password[0] : "",
+                                password: !!error.responseJSON.errors.password ? error.responseJSON.errors.password[0] : "",
+                                confirm_password: !!error.responseJSON.errors.confirm_password ? error.responseJSON.errors.confirm_password[0] : ""
+                            };
+                        }
+                        $scope.submitInProgress = false;
                     });
                 }
             };
