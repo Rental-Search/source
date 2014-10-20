11be1d1d42df1699d6fb3999482efb57cd7c9f80
benoitw <benoit.woj@gmail.com>
2014-10-20 11:08:37 +0200

    update static asset configuration

--- eloue/wsgi.py
+++ eloue/wsgi.py
@@ -6,6 +6,7 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eloue.settings")
 #os.environ['AWS_SECRET_ACCESS_KEY'] = 'EidEX/OtmAyUlVMdRzqdxL7RsPD2n0hp6BGZGvFF'
 
 from django.core.wsgi import get_wsgi_application
-from dj_static import Cling
+from whitenoise.django import DjangoWhiteNoise
 
-application = Cling(get_wsgi_application())
+application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
