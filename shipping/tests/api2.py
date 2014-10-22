ab15faed2066e0fe1a948f50ae3f2b2fa060173a
Pavel Antonov <pavel.antonov@noveogroup.com>
2014-10-22 17:38:45 +0700

    Several small tests added

--- shipping/tests/api2.py
+++ shipping/tests/api2.py
@@ -33,3 +33,42 @@ class ShippingPointTest(APITestCase):
         })
         self.assertEqual(response.status_code, status.HTTP_200_OK, response)
         self.assertGreater(len(response.data), 0, response.data)
+
+
+class PatronShippingPointTest(APITestCase):
+
+    fixtures = ['patron']
+
+    def setUp(self):
+        self.model = get_model('shipping', 'PatronShippingPoint')
+        self.client.login(username='alexandre.woog@e-loue.com', password='alexandre')
+
+    def test_create_point(self):
+        response = self.client.post(_location('patronshippingpoint-list'), {
+            'lat': 48.856614,
+            'lng': 2.3522219000000177,
+            'site_id': 123456,
+            'pudo_id': '123456',
+            'type': 1
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response)
+
+
+class ProductShippingPointTest(APITestCase):
+
+    fixtures = ['patron']
+
+    def setUp(self):
+        self.model = get_model('shipping', 'PatronShippingPoint')
+        self.client.login(username='alexandre.woog@e-loue.com', password='alexandre')
+
+    def test_create_point(self):
+        response = self.client.post(_location('patronshippingpoint-list'), {
+            'product': _location('product-detail', pk=1),
+            'lat': 48.856614,
+            'lng': 2.3522219000000177,
+            'site_id': 123456,
+            'pudo_id': '123456',
+            'type': 1
+        })
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED, response)
