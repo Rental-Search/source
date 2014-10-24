48676dfd675cf8a00fd1b5be1752eb124f6e3bf7
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2014-10-24 15:37:09 +0700

    Disabled availability dates filtering on search results page

--- products/forms.py
+++ products/forms.py
@@ -132,8 +132,8 @@ class FacetedSearchForm(SearchForm):
 class ProductFacetedSearchForm(FacetedSearchForm):
     price_from = forms.DecimalField(decimal_places=2, max_digits=10, min_value=D('0.01'), required=False)
     price_to = forms.DecimalField(decimal_places=2, max_digits=10, min_value=D('0.01'), required=False)
-    date_from = forms.DateField(input_formats=DATE_FORMAT, required=False)
-    date_to = forms.DateField(input_formats=DATE_FORMAT, required=False)
+#     date_from = forms.DateField(input_formats=DATE_FORMAT, required=False)
+#     date_to = forms.DateField(input_formats=DATE_FORMAT, required=False)
 
     def clean_price_from(self):
         price_from = self.cleaned_data.get('price_from', None)
@@ -157,27 +157,27 @@ class ProductFacetedSearchForm(FacetedSearchForm):
             return
         return price_to
 
-    def clean_date_from(self):
-        date_from = self.cleaned_data.get('date_from', None)
-        if date_from is None:
-            return
-        date_to = self.cleaned_data.get('date_to', None)
-        if (date_to is not None and date_from is not None and
-            date_from > date_to
-        ):
-            return
-        return min(date_from, date.today())
-
-    def clean_date_to(self):
-        date_to = self.cleaned_data.get('date_to', None)
-        if date_to is None:
-            return
-        date_from = self.cleaned_data.get('date_from', None)
-        if (date_to is not None and date_from is not None and
-            date_to < date_from
-        ):
-            return
-        return min(date_to, date.today())
+#     def clean_date_from(self):
+#         date_from = self.cleaned_data.get('date_from', None)
+#         if date_from is None:
+#             return
+#         date_to = self.cleaned_data.get('date_to', None)
+#         if (date_to is not None and date_from is not None and
+#             date_from > date_to
+#         ):
+#             return
+#         return min(date_from, date.today())
+#
+#     def clean_date_to(self):
+#         date_to = self.cleaned_data.get('date_to', None)
+#         if date_to is None:
+#             return
+#         date_from = self.cleaned_data.get('date_from', None)
+#         if (date_to is not None and date_from is not None and
+#             date_to < date_from
+#         ):
+#             return
+#         return min(date_to, date.today())
 
     def filter_queryset(self, sqs):
         sqs = super(ProductFacetedSearchForm, self).filter_queryset(sqs)
@@ -187,10 +187,10 @@ class ProductFacetedSearchForm(FacetedSearchForm):
         if self.cleaned_data['price_to'] is not None:
             sqs = sqs.filter(price__lte=self.cleaned_data['price_to'])
 
-        if self.cleaned_data['date_from'] is not None:
-            sqs = sqs.filter(created_at_date__gte=self.cleaned_data['date_from'])
-        if self.cleaned_data['date_to'] is not None:
-            sqs = sqs.filter(created_at_date__lte=self.cleaned_data['date_to'])
+#         if self.cleaned_data['date_from'] is not None:
+#             sqs = sqs.filter(created_at_date__gte=self.cleaned_data['date_from'])
+#         if self.cleaned_data['date_to'] is not None:
+#             sqs = sqs.filter(created_at_date__lte=self.cleaned_data['date_to'])
 
         return sqs
 
