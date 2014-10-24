cdb240528a0d18983da12718f67af9c9fa59660c
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2014-10-24 14:51:18 +0700

    Integrated rental agreement static page

--- eloue/urls.py
+++ eloue/urls.py
@@ -220,6 +220,7 @@ ui3_urlpatterns = patterns('',
     url(r'^qui-sommes-nous/', TemplateView.as_view(template_name='who_are_we/index.jade'), name='who_are_we'),
     url(r'^securite/', TemplateView.as_view(template_name='security/index.jade'), name='security'),
     url(r'^conditions-generales/', TemplateView.as_view(template_name='terms/index.jade'), name='terms'),
+    url(r'^contrat-de-location/', TemplateView.as_view(template_name='rental_agreement/index.jade'), name='agreement'),
 )
 
 urlpatterns = patterns('',
