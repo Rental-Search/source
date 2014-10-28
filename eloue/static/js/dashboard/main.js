9314d6c90e04e35ee1786c5325d9e932294c0694
Maxim Baev <mbaev@noveogroup.com>
2014-10-28 19:25:59 +0600

    Added scripts for Google Analytics

--- eloue/static/js/dashboard/main.js
+++ eloue/static/js/dashboard/main.js
@@ -81,5 +81,25 @@ require([
     $(function () {
 
         angular.bootstrap(document, ["EloueDashboardApp"]);
+
+        var load_google_analytics = function () {
+            var script = document.createElement("script");
+            script.type = "text/javascript";
+            var code = "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){" +
+                "(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o)," +
+                "m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)" +
+                "})(window,document,'script','//www.google-analytics.com/analytics.js','ga');" +
+                "ga('create', 'UA-8258979-7', 'e-loue.com');" +
+                "ga('send', 'pageview');";
+            try {
+                script.appendChild(document.createTextNode(code));
+                document.body.appendChild(script);
+            } catch (e) {
+                script.text = code;
+                document.body.appendChild(script);
+            }
+        };
+
+        load_google_analytics();
     });
 });
