18bdc2ef4c4e79142718bcac5a81ef532cd0b90b
Pavel Antonov <pavel.antonov@noveogroup.com>
2014-11-06 16:00:31 +0600

    #366 fixed for master branch

--- accounts/templatetags/patron.py
+++ accounts/templatetags/patron.py
@@ -11,5 +11,5 @@ register = template.Library()
 @stringfilter
 def hostname(value):
     data = urlparse.urlsplit(value)
-    return urlparse.urlunsplit(None, data[1], data[2], None, None) if data[2].strip('/') else escape(data[1])
+    return urlparse.urlunsplit((None, data[1], data[2], None, None)) if data[2].strip('/') else escape(data[1])
 hostname.is_safe = True
