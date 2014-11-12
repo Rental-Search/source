9c121d41fd5508890290112a811a55c1084b3327
Evgeny Sizikov <evgeny.sizikov@noveogroup.com>
2014-11-12 21:22:38 +0600

    Added SASS styles which are different for GoSport vertical site

--- eloue/gosport.py
+++ eloue/gosport.py
@@ -2,3 +2,12 @@
 from eloue.settings import *
 
 SITE_ID = 13
+
+SESSION_COOKIE_DOMAIN = 'go-sport.fr' # FIXME!
+
+CACHE_MIDDLEWARE_KEY_PREFIX = 'gosport'
+
+TEMPLATE_DIRS = env('TEMPLATE_DIRS', (
+    local_path('templates/gosport'),
+    local_path('templates/'),
+))
