419fa320ae137e4f474eb432780686c122b2def2
Pavel Antonov <pavel.antonov@noveogroup.com>
2014-11-13 13:08:32 +0600

    #389 special postfix for css

--- eloue/gosport.py
+++ eloue/gosport.py
@@ -7,6 +7,10 @@ SESSION_COOKIE_DOMAIN = 'go-sport.fr' # FIXME!
 
 CACHE_MIDDLEWARE_KEY_PREFIX = 'gosport'
 
+for key in PIPELINE_CSS:
+    output_filename = PIPELINE_CSS[key]['output_filename'].replace('.css', '_gosport.css')
+    PIPELINE_CSS[key]['output_filename'] = output_filename
+
 TEMPLATE_DIRS = env('TEMPLATE_DIRS', (
     local_path('templates/gosport'),
     local_path('templates/'),
