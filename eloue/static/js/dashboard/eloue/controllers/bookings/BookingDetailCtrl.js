9885fc41b17ae5f97d08b1108e5d115c27d0c298
Maxim Baev <mbaev@noveogroup.com>
2014-11-26 17:49:17 +0600

    #407 Display spinner while shipping details are being loaded on booking details page

--- eloue/static/js/dashboard/eloue/controllers/bookings/BookingDetailCtrl.js
+++ eloue/static/js/dashboard/eloue/controllers/bookings/BookingDetailCtrl.js
@@ -82,6 +82,7 @@ define(["angular", "eloue/app"], function (angular) {
                     });
                 }
                 if ($scope.bookingDetails.with_shipping) {
+                    $scope.searchShippingPointsInProgres = true;
                     ProductShippingPointsService.getByProduct($scope.bookingDetails.product.id).then(function (productShippingPointData) {
                         //Show shipping choice only if there are existing product shipping points
                         if (!!productShippingPointData.results && productShippingPointData.results.length > 0) {
@@ -89,6 +90,7 @@ define(["angular", "eloue/app"], function (angular) {
                             PatronShippingPointsService.getByPatronAndBooking($scope.bookingDetails.borrower.id, $stateParams.uuid).then(function (patronShippingPointData) {
                                 if (!!patronShippingPointData.results && patronShippingPointData.results.length > 0) {
                                     $scope.arrival_point = patronShippingPointData.results[0];
+                                    $scope.searchShippingPointsInProgres = false;
                                 }
                             }, function (error) {
                                 $scope.handleResponseErrors(error);
@@ -145,6 +147,7 @@ define(["angular", "eloue/app"], function (angular) {
             $scope.handleResponseErrors = function (error) {
                 $scope.serverError = error.errors;
                 $scope.submitInProgress = false;
+                $scope.searchShippingPointsInProgres = false;
             };
 
 
