0e2904b8e40a66355500651b5b8c5f1f2d191a8c
Maxim Baev <mbaev@noveogroup.com>
2014-11-28 10:50:17 +0600

    #414 french version of errors:  - Added angular-translate plugin for widgets module.  - Changed validation error messages to use message codes from common i18n.js file with translations.

--- eloue/static/js/widgets/build.js
+++ eloue/static/js/widgets/build.js
@@ -21,6 +21,7 @@
         "angular-cookies": "../../bower_components/angular-cookies/angular-cookies",
         "angular-sanitize": "../../bower_components/angular-sanitize/angular-sanitize",
         "angular-i18n": "../../bower_components/angular-i18n/angular-locale_fr-fr",
+        "angular-translate": "../../bower_components/angular-translate/angular-translate.min",
         "moment": "../../bower_components/moment/moment",
         "angular-moment": "../../bower_components/angular-moment/angular-moment",
         "bootstrap-datepicker": "../../bower_components/bootstrap-datepicker/js/bootstrap-datepicker",
@@ -48,6 +49,7 @@
         "angular-cookies": ["angular"],
         "angular-sanitize": ["angular"],
         "angular-i18n": ["angular"],
+        "angular-translate": ["angular"],
         "angular-resource": ["angular"],
         "angular-moment": ["angular"],
         "angular-mocks": {
