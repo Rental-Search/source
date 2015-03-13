f33c0dda3727b50e5d31e10821f6bed327ea9eab
Elena Orekhova <elena.lilac@gmail.com>
2015-03-13 18:14:47 +0600

    [ELO-125] - removed circular imports

--- eloue/search.py
+++ eloue/search.py
@@ -1,27 +1,22 @@
 # -*- coding: utf-8 -*-
 
 from django.db.models.signals import post_save, post_delete
+from django.db.models.loading import get_model
 
 from queued_search.signals import QueuedSignalProcessor
 
-from accounts.models import Patron
-from products.models import (
-    Alert, Product, CarProduct,
-    RealEstateProduct, UnavailabilityPeriod
-)
-from rent.models import Booking
-
 
 class HaystackSignalProcessor(QueuedSignalProcessor):
-    senders = (Patron, Alert, Product, CarProduct, RealEstateProduct)
-    related_senders = (Booking, UnavailabilityPeriod)
+    #senders = (Patron, Alert, Product, CarProduct, RealEstateProduct)
+    related_senders = (('rent', 'Booking'), ('products', 'UnavailabilityPeriod'))
 
     def setup(self):
         post_save.connect(self.enqueue_save)
         post_delete.connect(self.enqueue_delete)
 
         # If booking or UnavailabilityPeriod was changed - update ProductIndex
-        for sender in self.related_senders:
+        for app, model in self.related_senders:
+            sender = get_model(app, model)
             post_save.connect(
                     self.enqueue_product_related_save, sender=sender)
             post_delete.connect(
@@ -31,7 +26,8 @@ class HaystackSignalProcessor(QueuedSignalProcessor):
         post_save.disconnect(self.enqueue_save)
         post_delete.disconnect(self.enqueue_delete)
 
-        for sender in self.related_senders:
+        for app, model in self.related_senders:
+            sender = get_model(app, model)
             post_save.disconnect(
                     self.enqueue_product_related_save, sender=sender)
             post_delete.disconnect(
