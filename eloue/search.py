9b7180622e3092c81e48f11abe43645260e6fa33
Elena Orekhova <elena.lilac@gmail.com>
2015-03-16 16:36:54 +0600

    [ELO-125] - removed circular imports from Haystack signal processor

--- eloue/search.py
+++ eloue/search.py
@@ -6,7 +6,10 @@ from queued_search.signals import QueuedSignalProcessor
 
 class HaystackSignalProcessor(QueuedSignalProcessor):
     #senders = (Patron, Alert, Product, CarProduct, RealEstateProduct)
-    related_senders = (('rent', 'Booking'), ('products', 'UnavailabilityPeriod'))
+    _SENDER_MAP = {
+        'Booking': 'product',
+        'UnavailabilityPeriod': 'product',
+    }
 
     def setup(self):
         post_save = models.signals.post_save
@@ -15,14 +18,6 @@ class HaystackSignalProcessor(QueuedSignalProcessor):
         post_save.connect(self.enqueue_save)
         post_delete.connect(self.enqueue_delete)
 
-        # If booking or UnavailabilityPeriod was changed - update ProductIndex
-        for app, model in self.related_senders:
-            sender = models.loading.get_model(app, model)
-            post_save.connect(
-                    self.enqueue_product_related_save, sender=sender)
-            post_delete.connect(
-                    self.enqueue_product_related_delete, sender=sender)
-
     def teardown(self):
         post_save = models.signals.post_save
         post_delete = models.signals.post_delete
@@ -30,15 +25,12 @@ class HaystackSignalProcessor(QueuedSignalProcessor):
         post_save.disconnect(self.enqueue_save)
         post_delete.disconnect(self.enqueue_delete)
 
-        for app, model in self.related_senders:
-            sender = models.loading.get_model(app, model)
-            post_save.disconnect(
-                    self.enqueue_product_related_save, sender=sender)
-            post_delete.disconnect(
-                    self.enqueue_product_related_delete, sender=sender)
+    def enqueue_save(self, sender, instance, **kwargs):
+        if sender.__name__ in self._SENDER_MAP:
+            instance = getattr(instance, self._SENDER_MAP[sender.__name__], instance)
+        return self.enqueue('update', instance)
 
-    def enqueue_product_related_save(self, sender, instance, **kwargs):
-        return self.enqueue('update', instance.product)
-
-    def enqueue_product_related_delete(self, sender, instance, **kwargs):
-        return self.enqueue('delete', instance.product)
+    def enqueue_delete(self, sender, instance, **kwargs):
+        if sender.__name__ in self._SENDER_MAP:
+            instance = getattr(instance, self._SENDER_MAP[sender.__name__], instance)
+        return self.enqueue('delete', instance)
