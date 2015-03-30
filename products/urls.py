389d28bf02d33443933c4a620a8f00ee762f0c80
Elena Orekhova <elena.lilac@gmail.com>
2015-03-30 12:33:29 +0600

    [ELO-70] - initial

--- products/urls.py
+++ products/urls.py
@@ -6,8 +6,9 @@ from django.utils.translation import ugettext as _
 
 from .search import product_search
 from .views import (
-    ProductListView, ProductDetailView, PublishItemView, SuggestCategoryView
+    ProductListView, ProductDetailView, PublishItemView, SuggestCategoryView, CategoryDetailView
 )
+
 
 product_extra_context = {'sqs': product_search}
 
@@ -17,4 +18,6 @@ urlpatterns = patterns('',
     url(r'^%s/$' % _('ajouter'), PublishItemView.as_view(), name='publish_item'),
     url(r'^([^/].+/)(?P<slug>[-\w]+)-(?P<pk>\d+)/$', ProductDetailView.as_view(), product_extra_context, name='booking_create'),
     url(r'^([^/].+/)?$', ProductListView.as_view(), product_extra_context, name='product_list'),
+    url(r'^category/(?P<slug>[-\w]+)/$', CategoryDetailView.as_view(),
+        name='category_detail'),
 )
