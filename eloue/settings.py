933240a00267f76790445613b53be90ed0d4d98e
benoitw <benoit.woj@gmail.com>
2015-08-27 14:24:25 +0200

    modify the makemessages command for our app

--- eloue/settings.py
+++ eloue/settings.py
@@ -244,6 +244,7 @@ INSTALLED_APPS = (
     'eloue.api',
     'south', # South must be the last in the list of applications that contains models
     'django_nose', # Make sure that django-nose comes after south in INSTALLED_APPS so that django_nose's test command is used.
+    'core',
 )
 
 if DEBUG_TOOLBAR:
