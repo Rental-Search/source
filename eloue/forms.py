cdc7ce0c386a5c0fbdbcb52c7dfdf2e4fc348418
hugoeloue <hugo.woog@e-loue.com>
2015-09-04 18:14:07 +0200

    form set in html

--- eloue/forms.py
+++ eloue/forms.py
@@ -3,5 +3,5 @@ from django import forms
 class ContactForm(forms.Form):
 	message = forms.CharField(widget=forms.Textarea)
 	sender = forms.EmailField(label='Your name', max_length=100)
-	category = forms.CharField(label='objet', max_length=100)
+	category = forms.CharField(widget=forms.Select)
 	cc_myself = forms.BooleanField(required=False)
--- eloue/views.py
+++ eloue/views.py
@@ -150,28 +150,21 @@ class ContactView(View):
                 try:
                     send_mail(subject, message, sender, recipients)
                 except BadHeaderError:
-                    messages.add_message(request, messages.INFO, '<div id="toast-container" class="toast-top-full-width" aria-live="polite" role="alert"><div class="toast toast-error"><div class="toast-message">Erreur dans le formulaire</div></div></div>', extra_tags='safe')
+                    tag = "error"
+                    messages.add_message(request, messages.ERROR, _('Erreur dans le formulaire3'), extra_tags='safe')
                     return render(request, self.template_name, {'form': form})
-                messages.add_message(request, messages.INFO, '<div id="toast-container" class="toast-top-full-width" aria-live="polite" role="alert"><div class="toast toast-success"><div class="toast-message">Votre message a bien été envoyé</div></div></div>', extra_tags='safe')
+                tag = "success"
+                messages.add_message(request, messages.SUCCESS, _('Le message a ete envoye avec succes'), extra_tags='safe')
                 return render(request, self.template_name, {'form': new_form})
             else:
-                messages.add_message(request, messages.INFO, '<div id="toast-container" class="toast-top-full-width" aria-live="polite" role="alert"><div class="toast toast-error"><div class="toast-message">Erreur dans le formulaire</div></div></div>', extra_tags='safe')
+                tag = "error"
+                messages.add_message(request, messages.ERROR, _('Erreur dans le formulaire1'), extra_tags='safe')
                 return render(request, self.template_name, {'form': form})
         
         else:
-            messages.add_message(request, messages.INFO, '<div id="toast-container" class="toast-top-full-width" aria-live="polite" role="alert"><div class="toast toast-error"><div class="toast-message">Form not valiv</div></div></div>', extra_tags='safe')
+            tag = "error"
+            messages.add_message(request, messages.ERROR, _('Erreur dans le formulaire2'), extra_tags='safe')
             return render(request, self.template_name, {'form': form})
-
-
-
-
-
-
-
-
-
-
-
 
 
 
