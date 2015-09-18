3a4ba3f91cbd4a52a0ae2b2364e1cdb3e9550ac6
benoitw <benoit.woj@gmail.com>
2015-09-18 12:19:14 +0200

    delete old stuff

--- eloue/static/proapp/js/models/redirectionevents.js
+++
@@ -1,9 +0,0 @@
-// js/models/redirectionevent.js
-
-var app = app || {};
-
-app.RedirectionEventModel = Backbone.Model.extend({
-
-	urlRoot: API_URL.redirection_events + "analytics/",
-
-});
--- eloue/static/proapp/js/models/trafficevents.js
+++
@@ -1,9 +0,0 @@
-// js/models/trafficevent.js
-
-var app = app || {};
-
-app.TrafficEventModel = Backbone.Model.extend({
-
-	urlRoot: API_URL.traffic_events + "analytics/",
-
-});
