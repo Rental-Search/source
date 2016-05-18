c12755f15f7bf56e64107f98d8e3a22c106ccc1c
Ivan <ivan.melnychenko@e-loue.com>
2016-05-18 10:52:30 +0300

    reset page #131970695463387, scroll page up #112057705511662

--- eloue/static/js/widgets/eloue/controllers/AlgoliaProductListCtrl.js
+++ eloue/static/js/widgets/eloue/controllers/AlgoliaProductListCtrl.js
@@ -670,8 +670,13 @@ define([
                     } else {
                         $scope.search.location_ui_changed = false;
                     }
+                    
                 };
                 
+                $scope.refineQuery = function(){
+                    $scope.search.page = 0;
+                    $scope.perform_search();  
+                };
                 
                 $scope.refinePrices = function(sliderId){ //$log.debug('refinePrices');
                     
@@ -807,6 +812,7 @@ define([
                 $scope.setPage = function(page){ //$log.debug('setPage');
                     $scope.search.page = page;
                     $scope.perform_search();
+                    $window.scrollTo(0, 0);
                 };
                 
                 $scope.setOrdering = function(ordering){ //$log.debug('setOrdering');
