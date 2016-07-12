712816e427bc55da7eda3df23d5da1dbf8fe7994
benoitw <benoit.woj@gmail.com>
2016-07-12 16:57:00 +0200

    refactor the importgame

--- products/management/commands/import_game2016.py
+++ products/management/commands/import_game2016.py
@@ -7,6 +7,8 @@ from accounts.models import Patron
 from accounts.models import PhoneNumber
 from urllib2 import urlopen
 import tempfile
+from django.template.defaultfilters import slugify
+
 
 class Command(BaseCommand):
 	help = "import User for euro game 2016"
@@ -19,7 +21,6 @@ class Command(BaseCommand):
 	def handle(self, *args, **options):
 
 		# Read file data
-
 		f = urlopen("http://eloue.s3.amazonaws.com/base_jeuconcourseuro_20160622.xls")
 		content = xlrd.open_workbook(file_contents=f.read())
 		f.close()
@@ -32,14 +33,7 @@ class Command(BaseCommand):
 			try:
 				phonenumber =  str(int(sh.row(x)[5].value))
 			except Exception, e:
-				phonenumber = "0000000000"
-			
-			try:
-				zipcode = str(int(sh.row(x)[6].value))
-			except Exception, e:
-				zipcode = "00000"
-				pass
-
+				phonenumber = None
 
 			Password = 'JEUEURO2016'
 
@@ -52,40 +46,39 @@ class Command(BaseCommand):
 			
 			#print username
 			# Add 0 to phonenumber
-			phonenumber = '0' +  phonenumber
+			if phonenumber:
+				phonenumber = '0' +  phonenumber
+			
 			# Check User exist
-			try:
-				a = Patron.objects.get(email=email)
-				self.isUserExist = True
-			except Exception, e:
-				self.isUserExist = False
-				print email
-				
-			if username in self.userlist:
-				self.usernameExist_count += 1
-				username = username + '_' + str(self.usernameExist_count)
-				self.userlist.add(username)
-			else:
-				self.usernameExist_count = 0
-				self.userlist.add(username)
+			patron_count = Patron.objects.filter(email=email).count()
 
-			if (self.isUserExist == False):
+			if patron_count == 0:
+
+				#Check if the username exist
+				patrons = Patron.objects.filter(username__startswith=username)
+				if patrons.count() > 0:
+					username = username + "%s" % patrons.count()
+
+				#Check if the slug exist
+				slug=slugify(username)
+				patrons = Patron.objects.filter(slug__startswith=slug)
+				if patrons.count() > 0:
+					slug = slug + "%s" % patrons.count()
+
 				try:
-					newuser = Patron.objects.create(email=email, username=username, slug=username,
+					newuser = Patron.objects.create(email=email, username=username, slug=slug,
 						last_name=lastname, first_name=firstname)
 					newuser.set_password(Password)
 
 					# Add phone number
-					newphone = PhoneNumber()
-					newphone.id = newuser.id
-					newphone.number = phonenumber
-					newphone.kind = 2
-					newuser.phones.add(newphone)
-
+					if phonenumber:
+						newuser.phones.add(PhoneNumber(number=phonenumber))
+					
 					newuser.save()
 					self.newuser_count += 1
+					print newuser.email
 				except Exception, e:
-					#print e
+					print e
 					pass
 
 		print "Total new user added : %d" % self.newuser_count
