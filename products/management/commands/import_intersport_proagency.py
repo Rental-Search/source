c6c34ff16db23841b1153334548c2d7e728090b1
benoitw <benoit.woj@gmail.com>
2016-09-06 11:43:23 +0200

    refactor import intersport agencies

--- products/management/commands/import_intersport_proagency.py
+++ products/management/commands/import_intersport_proagency.py
@@ -16,7 +16,7 @@ class Command(BaseCommand):
 
 		nb_proagency = 0
 		# Read file data
-		f = urlopen("http://eloue.s3.amazonaws.com/intersport_agencies.xlsx")
+		f = urlopen("http://eloue.s3.amazonaws.com/intersport_agencies2.xlsx")
 		content = xlrd.open_workbook(file_contents=f.read())
 		f.close()
 		sh = content.sheet_by_index(0)
@@ -32,15 +32,21 @@ class Command(BaseCommand):
 			except Exception, e:
 				zipcode = sh.row(x)[3].value
 				pass
+
+			try:
+				city = str(sh.row(x)[4].value)
+			except Exception, e:
+				city = sh.row(x)[4].value
+				pass
 			
 			try:
-				phone_number = str(int(sh.row(x)[4].value))
+				phone_number = str(int(sh.row(x)[5].value))
 				phone_number = '0' + phone_number
 			except Exception, e:
 				pass
 			
 			try:
-				newAgency = ProAgency.objects.create(name=name, address1=address, zipcode=zipcode, phone_number=phone_number, patron_id=patron_id)
+				newAgency = ProAgency.objects.create(name=name, address1=address, city=city, zipcode=zipcode, phone_number=phone_number, patron_id=patron_id)
 				nb_proagency += 1
 			except Exception, e:
 				raise e
@@ -48,5 +54,6 @@ class Command(BaseCommand):
 		print 'Total proagency added : %s' % str(nb_proagency)
 			# print 'Name: %s' % name
 			# print 'address: %s' % address
+			# print 'city: %s' % city
 			# print 'Zipcode: %s' % zipcode
-			# print 'Phone_number: %s' % phone_number 
+			# print 'Phone_number: %s' % phone_number
