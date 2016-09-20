46eef731cb32ea1ad814eeb78444bd4f443056c1
Ivan <ivan.melnychenko@e-loue.com>
2016-09-20 13:03:41 +0300

    added debugging code to manage.py

--- manage.py
+++ manage.py
@@ -3,6 +3,17 @@
 import os
 import sys
 
+
+if bool(os.getenv("PYDEV_DEBUG", False)):
+    print("Connecting to pydev debug server...")
+    sys.path.append(os.getenv("PYDEV_PATH"), 'pysrc/pycharm-debug.egg')
+    import pydevd
+    pydevd.settrace(os.getenv("PYDEV_DEBUG_SERVER", '172.18.0.1'),
+                    port=int(os.getenv("PYDEV_DEBUG_PORT", '5678')),
+                    suspend=False, stdoutToServer=True, stderrToServer=True)
+    print("Connected.")
+
+
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eloue.settings")
 
@@ -12,7 +23,9 @@ if __name__ == "__main__":
         os.environ['AWS_ACCESS_KEY_ID'] = settings.AWS_ACCESS_KEY_ID
         os.environ['AWS_SECRET_ACCESS_KEY'] = settings.AWS_SECRET_ACCESS_KEY
     except ImportError:
-        sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n" % __file__)
+        sys.stderr.write("""Error: Can't find the file 'settings.py' in the directory
+containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your
+settings module.\n(If the file settings.py does indeed exist, it's causing an ImportError somehow.)\n""" % __file__)
         sys.exit(1)
 
     from django.core.management import execute_from_command_line
